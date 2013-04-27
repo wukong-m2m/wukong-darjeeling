ec8a41f6f7cba3fa935deb85e7a4bc7079016f99
wycc <wycca1@gmail.com>
2013-04-27 23:38:33 +0800

    First version of the WuType editor

--- wukong/master/static/js/wuide.js
+++ wukong/master/static/js/wuide.js
@@ -1,3 +1,4 @@
+// vim: ts=4 sw=4
 $(document).ready(function() {
 	ide = new WuIDE();
 });
@@ -11,9 +12,47 @@ function WuIDE()
 		self.xml = $.parseXML(r);
 		self.classes = $(self.xml).find('WuClass');
 		self.types = $(self.xml).find('WuTypedef');
+		self.parseXML();
 		self.load();
 	});
 	this.initUI();
+}
+
+WuIDE.prototype.parseXML = function() {
+	var classes =$(this.xml).find('WuClass');
+	var types =$(this.xml).find('WuTypedef');
+	var self = this;
+
+	self.classes = [];
+	$.each(classes, function(i,val) {
+		var name = $(val).attr('name');
+		var id = $(val).attr('id');
+		var virtual = $(val).attr('virtual');
+		var type = $(val).attr('hard');
+		var properties = $(val).find('property');
+		var prop = [];
+		$.each(properties, function(j, val) {
+			var pname = $(val).attr('name');
+			var datatype=$(val).attr('datatype');
+			var access = $(val).attr('access');
+			var def = $(val).attr('default');
+			prop.push({name:pname, datatype:datatype, access:access, default:def});
+		});
+		self.classes.push({name:name, id:id, virtual:virtual,type:type});
+	});
+	self.types=[];
+	$.each(types,function(i,v) {
+		var name=$(v).attr('name');
+		var type=$(v).attr('type');
+		var enumlist=[];
+		if (type == 'enum') {
+			var enums = $(v).find('enum');
+			$.each(enums,function(j,v) {
+				enumlist.push({name:$(v).attr('value')});
+			});
+			self.types.push({name:name,type:type,enums:enumlist});
+		}
+	});
 }
 
 WuIDE.prototype.initUI = function() {
@@ -25,19 +64,46 @@ WuIDE.prototype.initUI = function() {
 WuIDE.prototype.load = function() {
 	var self = this;
 	var cont = $('#types');
-	cont.empty();
-	cont.append('<table id=type_list></table>');
-	$.each(this.types, function(i,val) {
-		$('#type_list').append('<tr id=type'+i+'><td>'+$(val).attr('name')+'</td><td><button id=type'+i+'_edit>Edit</button></td></tr>'); 
-	});
-	
-	cont = $('#classes');
-	cont.empty();
-	cont.append('<table id=class_list></table>');
-	$.each(this.classes, function(i,val) {
-		$('#class_list').append('<tr id=type'+i+'><td>'+$(val).attr('name')+'</td><td><button id=type'+i+'_edit>Edit</button></td></tr>'); 
+	var i;
+
+	var data = {types:self.types,classes:self.classes};
+	if (self.typelistTemplate == null) {
+		this.typelistTemplate=$('#type_list').compile({
+			'tr._type': {
+				'type <- types': {
+					'td._name':'type.name',
+					'button._edit@id': function(arg) {
+						return 'type'+arg.pos+'_edit';
+					},
+					'button._del@id':function(arg) {
+						return 'type'+arg.pos+'_del';
+					}
+				}
+			}
+		});
+	}
+	$('#type_list').empty();
+	$('#type_list').render(data,this.typelistTemplate);
+	$.each(self.types,function(i,val) {
 		self.installTypeEditor(i);
 	});
+	$('#addtype').click(function() {
+		self.types.push({type:'enum',name:'New Type',enums:[]});
+		self.load();
+	});
+		
+	
+	if (self.classListTemplate == null) {
+		this.classListTemplate=$('#classes').compile({
+			'tr._class': {
+				'class <- classes': {
+					'td._name':'class.name',
+					'button._edit@id':'class.id'
+				}
+			}
+		});
+	}
+	$('#classes').render(data,this.classListTemplate);
 }
 
 WuIDE.prototype.installTypeEditor=function(i) {
@@ -45,22 +111,26 @@ WuIDE.prototype.installTypeEditor=function(i) {
 	$('#type'+i+'_edit').click(function() {
 		self.editType(i);
 	});
+	$('#type'+i+'_del').click(function() {
+		self.types.splice(i,1);
+		self.load();
+	});
 }
 
 WuIDE.prototype.showEnumNameEditor = function(item,i) {
 	$('body').append('<div id=enumname></div>');
 	var dialog = $('#enumname');
-	var enums = $(item).find('enum');
 	dialog.append('<H2>Name</H2>');
 	dialog.append('<input type=text id=enumname_text></input>');
-	$('#enumname_text').val($(enums[i]).attr('value'));
+	$('#enumname_text').val(item.enums[i].name);
 	dialog.dialog({
 		title: 'ENUM editor',
 		autoOpen: true,
 		buttons: {
 			'OK': function () {
 				// Update the XML here
-				$('#type'+i+'_name').text($('#enumname_text').val());
+				item.enums[i].name = $('#enumname_text').val();
+				$('#enum'+i+' td._name').text($('#enumname_text').val());
 				dialog.dialog('close');
 				dialog.remove();
 			},
@@ -74,36 +144,63 @@ WuIDE.prototype.showEnumNameEditor = function(item,i) {
 
 WuIDE.prototype.installEnumEditor = function(item,i) {
 	var self = this;
-	$('#type'+i+'_del').click(function() {
-		$('#type'+i).remove();
-		// remove item from XML
+	$('#delenum'+i).click(function() {
+		item.enums.splice(i,1);
+		$('#type_editor').empty();
+		$('#type_editor').render(item, self.editTypeTemplate);
+		$.each(item.enums, function(i,val) {
+			self.installEnumEditor(item,i);
+		});
+		$('#type_editor').show();
 	});
-	$('#type'+i+'_edit').click(function() {
+	$('#editenum'+i).click(function() {
 		self.showEnumNameEditor(item,i);
 	});
 }
 
-WuIDE.prototype.editType = function(i) {
+WuIDE.prototype.refreshEnumList = function(item,i) {
 	var self = this;
-	$('#types').empty();
-	var item = this.types[i];
-	var enums = $(item).find('enum');
-	
-	$('#types').append('<button id=editdone>Done</button>');
-	$('#types').append('<button id=addenum>Add</button>');
-	$('#editdone').click(function() {
+	if (this.editTypeTemplate == null) {
+		this.editTypeTemplate = $('#type_editor').compile({
+			'tr._class': {
+				'e <- enums': {
+					'td._name':'e.name',
+					'button._edit@id': function(arg) {
+						return 'editenum'+arg.pos;
+					},
+					'button._del@id': function(arg) {
+						return 'delenum'+arg.pos;
+					},
+					'.@id': function(arg) {
+						return 'enum'+arg.pos;
+					}
+				},
+			},
+		});
+	}
+	$('#type_editor').empty();
+	$('#type_editor').render(item, this.editTypeTemplate);
+	$.each(item.enums, function(i,val) {
+		self.installEnumEditor(item,i);
+	});
+	$('#typeeditdone').unbind().click(function() {
 		// Update the XML here
+		$('#type_editor').hide();
+		$('#type_list').show();
+		item.name = $('#type_editor_name').val();
 		self.load();
 	});
-	$('#addenum').click(function() {
-		var i = enums.length+1;
-		$('#type_editor').append('<tr id=type'+i+'><td>Dummy</td><td><button id=type'+i+'_edit>Edit</button></td><td><button id=type'+i+'_del>Delete</button></td></tr>'); 
-		$(item).append('<enum value=dummy></enum>');
-		self.installEnumEditor(item,i);
+	$('#addenum').unbind().click(function() {
+		item.enums.push({name:'Dummy'});
+		self.refreshEnumList(item,i);
 	});
-	$('#types').append('<table id=type_editor></table>');
-	$.each(enums, function(i,val) {
-		$('#type_editor').append('<tr id=type'+i+'><td id=type'+i+'_name>'+$(val).attr('value')+'</td><td><button id=type'+i+'_edit>Edit</button></td><td><button id=type'+i+'_del>Delete</button></td></tr>'); 
-		self.installEnumEditor(item,i);
-	});
+	$('#type_editor').show();
+}
+WuIDE.prototype.editType = function(i) {
+	var self = this;
+	var item = this.types[i];
+	var enums = item.enums;
+	$('#type_list').hide();
+	this.refreshEnumList(item,i);
+	$('#type_editor_name').val(this.types[i].name);
 }
