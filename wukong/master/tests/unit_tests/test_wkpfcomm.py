1b5c593ff65960bc1554a6f3d46c1da7d0e10e1a
Penn Su <pennsu@gmail.com>
2013-08-04 23:16:43 +0800

    Delete unused files

--- wukong/master/tests/unit_tests/test_wkpfcomm.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-import unittest
-import test_environment
-from wkpf.wkpfcomm import *
-
-# test setProperty
