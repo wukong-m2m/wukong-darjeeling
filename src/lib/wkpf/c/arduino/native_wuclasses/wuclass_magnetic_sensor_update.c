88d51cb44d11f0efb2672a3f41f83f55af5c2a82
Niels Reijers <nielsreijers@gmail.com>
2013-11-10 15:13:36 +0800

    Added wuclass for magnetic sensor

---
+++ src/lib/wkpf/c/arduino/native_wuclasses/wuclass_magnetic_sensor_update.c
@@ -0,0 +1,24 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+#include <avr/io.h>
+#include "../../common/native_wuclasses/GENERATEDwuclass_magnetic_sensor.h"
+
+#define set_input(portdir, pin) portdir &= ~(1<<pin)
+#define output_high(port, pin) port |= (1<<pin)
+#define input_get(port, pin) ((port & (1 << pin)) != 0)
+
+#ifdef ENABLE_WUCLASS_MAGNETIC_SENSOR
+
+void wuclass_magnetic_sensor_setup(wuobject_t *wuobject) {
+  DEBUG_LOG(true, "WKPFUPDATE(MagneticSensor): setup\n");
+  set_input(DDRE, 4);
+  output_high(PINE, 4);
+}
+
+void wuclass_magnetic_sensor_update(wuobject_t *wuobject) {
+  bool currentValue = input_get(PINE, 4);
+  DEBUG_LOG(true, "WKPFUPDATE(MagneticSensor): Sensed binary value: %d\n", currentValue);  
+  wkpf_internal_write_property_boolean(wuobject, WKPF_PROPERTY_MAGNETIC_SENSOR_OUTPUT, currentValue);  
+}
+
+#endif // ENABLE_WUCLASS_MAGNETIC_SENSOR
