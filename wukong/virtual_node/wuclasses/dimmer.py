e10d265ef4fc82228f8f8502a987b08c3c7d6036
余士元 <louis29418401@gmail.com>
2013-12-02 18:06:05 +0800

    dimmer wuclass implemented

---
+++ wukong/virtual_node/wuclasses/dimmer.py
@@ -0,0 +1,19 @@
+class Dimmer(VirtualNodeWuClass):
+
+	def setup(self, comm):
+		self.comm = comm
+
+	def update(self, properties, links):
+
+		on_off = properties.get("on_off")
+		level = properties.get("level")
+
+		if on_off is True:
+			if self.port_number == 70:
+				reply = self.comm.zwave.send_raw(node.id, [0x20, 0x01, level,])
+			elif self.port_number == 71:
+				reply = self.comm.zwave.send_raw(node.id, [0x60,0x0d,0x1,0x2,0x20, 0x01, level,])
+			elif self.port_number == 72:
+				reply = self.comm.zwave.send_raw(node.id, [0x60,0x0d,0x1,0x3,0x20, 0x01, level,])
+		
+
