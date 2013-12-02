328efc1ef84dae2d79a574e8a45276b05eeb786b
余士元 <louis29418401@gmail.com>
2013-12-02 17:15:06 +0800

    threshold wuclass implemented

---
+++ wukong/virtual_node/wuclasses/threshold.py
@@ -0,0 +1,24 @@
+
+LT=0
+GT=1
+LTE = 2
+GTE = 3
+
+class Threshold: 
+	def __init__(self):
+		pass
+	
+	def setup(self):
+		pass
+
+	def update(self, properties, links):
+
+		threshold = properties.get("threshold")
+		operator = properties.get("operator")
+ 		value = properties.get("value")
+
+		if(((operator == GT && operator == GTE)) and value > threshold) or (((operator == LT && operator == LTE)) and value < threshold) or (((operator == GTE && operator == LTE)) and value == threshold):
+			properties.set("output", True)
+		else:
+			properties.set("output", False)
+
