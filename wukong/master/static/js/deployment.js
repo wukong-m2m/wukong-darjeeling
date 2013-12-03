b5432a3749f51683abc508583ff06dfcf77ef2d1
Penn Su <pennsu@gmail.com>
2013-12-03 12:29:46 +0800

    Revert "Extrapolate poll functions into a file"
    This reverts commit f16ea48e2312db7bff4023ecb77150eff3fd2f16.

--- wukong/master/static/js/deployment.js
+++ wukong/master/static/js/deployment.js
@@ -1,3 +1,88 @@
+global_polling_status = false;
+
+function polling_is_stopped() {
+    return !global_polling_status
+}
+
+function start_polling()
+{
+    // start polling
+    window.options = {repeat: true};
+}
+
+function stop_polling()
+{
+    // stop polling
+    window.options = {repeat: false};
+}
+
+function application_polling(app_id, destination, property)
+{
+    // stops previous polling
+    stop_polling();
+
+    while (!polling_is_stopped()) {};
+
+    // starts a new one
+    start_polling();
+
+    // sets default destination
+    if (typeof destination == 'undefined') {
+        destination = '#mapping-progress';
+    }
+
+    // sets default property
+    if (typeof property == 'undefined') {
+        property = 'all_wukong_status';
+    }
+
+    poll('/applications/' + app_id + '/poll', 0, window.options, function(data) {
+        //data.wukong_status = data.wukong_status.trim();
+        //data.application_status = data.application_status.trim();
+        console.log(data);
+        $(destination).empty();
+        for (var i=0; i<data[property].length; i++) {
+            $(destination).append("<pre>[" + data[property][i].level + "] " + data[property][i].msg + "</pre>");
+        }
+        
+        //if (data.wukong_status === "close" || data.application_status === "close") {
+            //$('#deploy_results').dialog('close');
+        //} else if (!(data.wukong_status === "" && data.application_status === "")) {
+            //$('#deploy_results').dialog({modal: true, autoOpen: true, width: 600, height: 300}).dialog('open');
+            //$('#deploy_results #wukong_status').text(data.wukong_status);
+            //$('#deploy_results #application_status').text(data.application_status);
+        //}
+    });
+}
+
+// might have to worry about multiple calls :P
+function poll(url, version, options, callback)
+{
+    var forceRepeat = false;
+    if (typeof options != 'undefined') {
+        // repeat is an object with one key 'repeat', or it will be pass-by-value and therefore can't be stopped
+        forceRepeat = options.repeat;
+    }
+
+    global_polling_status = true;
+    console.log('polling');
+    $.post(url, {version: version}, function(data) {
+        if (typeof callback != 'undefined') {
+            callback(data);
+        }
+
+        if (data && data.ops && data.ops.indexOf('c') != -1) {
+            stop_polling();
+        } else if (forceRepeat) {
+            setTimeout(function() {
+                poll(url, data.version, options, callback);
+            }, 1000);
+        }
+
+        global_polling_status = false;
+    });
+}
+
 $(function() {
     $('#deployment-tab').click(function () {
         $('a#nodes-btn').click();
