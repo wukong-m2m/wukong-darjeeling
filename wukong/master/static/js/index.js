d6c9b4790e05e8aca41a8626a82f78a3bad04f45
Penn Su <pennsu@gmail.com>
2013-12-19 00:43:44 +0800

    It will check the return value of prompt so it will not create application if the name is empty or null

--- wukong/master/static/js/index.js
+++ wukong/master/static/js/index.js
@@ -35,14 +35,16 @@ app.controller('MainController', function($scope, $state, $log, Applications) {
   $scope.apps = Applications.query();
   $scope.create = function() {
     app_name = prompt('Please enter the application name:', 'Application name');
-    var newApp = new Applications({app_name: app_name});
-    $log.debug('creating');
-    newApp.$save({app_name: app_name}, function() {
-      $log.debug('saved');
-      $log.debug(newApp);
-      $scope.apps = Applications.query();
-      window.location.href = '/#/a/' + newApp.id; // angularjs routing prefix
-    });
+    if(app_name != '' && app_name != null) {
+      var newApp = new Applications({app_name: app_name});
+      $log.debug('creating');
+      newApp.$save({app_name: app_name}, function() {
+        $log.debug('saved');
+        $log.debug(newApp);
+        $scope.apps = Applications.query();
+        window.location.href = '/#/a/' + newApp.id; // angularjs routing prefix
+      });
+    }
   };
   $scope.remove = function(app) {
     app.$remove();
