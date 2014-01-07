dbdb065e57be9b0f4db0a83103aaf9a8bdc7808f
Niels Reijers <nielsreijers@gmail.com>
2014-01-07 17:38:58 +0800

    #Issue 122 Added empty application to start a fresh VM with. (wkemptyapp) A completely new download of the repo won't have anything in wkdeploy, so it won't build, and building the VM with helloworld doesn't start the profile framework.

---
+++ src/app/wkemptyapp/java/WKEmptyApp.java
@@ -0,0 +1,23 @@
+import javax.wukong.wkpf.*;
+import javax.wukong.virtualwuclasses.*;
+
+public class WKDeploy {
+    // Empty application to build an WuKong VM without any app.
+    public static void main (String[] args) {
+        System.out.println("My node id: " + WKPF.getMyNodeId());
+
+        WKPF.appLoadInitLinkTableAndComponentMap();
+        createVirtualWuObjects();
+        WKPF.appInitCreateLocalObjectAndInitValues();
+
+        while(true){
+            VirtualWuObject wuobject = WKPF.select();
+            if (wuobject != null) {
+                wuobject.update();
+            }
+        }
+    }
+
+    private static void createVirtualWuObjects() {
+    }
+}
