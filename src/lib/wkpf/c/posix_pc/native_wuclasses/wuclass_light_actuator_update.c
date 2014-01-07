89d609e0a568a8b4f7a348f224a0aadb3c92deb1
Niels Reijers <nielsreijers@gmail.com>
2014-01-07 14:46:33 +0800

    Issue #122. Added subplatform option to build process to support eBox.

---
+++ src/lib/wkpf/c/posix_pc/native_wuclasses/wuclass_light_actuator_update.c
@@ -0,0 +1,13 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+#include <stdio.h>
+
+void wuclass_light_actuator_setup(wuobject_t *wuobject) {}
+
+void wuclass_light_actuator_update(wuobject_t *wuobject) {
+	bool onOff;
+	wkpf_internal_read_property_boolean(wuobject, WKPF_PROPERTY_LIGHT_ACTUATOR_ON_OFF, &onOff);
+
+
+	printf("WKPFUPDATE(Light): Setting light to: %x\n", onOff);
+}
