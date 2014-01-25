87414af3dd88fa8f827655f1b33e0603fde3fd4a
Niels Reijers <nielsreijers@gmail.com>
2014-01-25 20:39:28 +0800

    Issue #127 Added some more wuclasses to posix_pc

---
+++ src/lib/wkpf/c/posix_pc/native_wuclasses/wuclass_temperature_humidity_sensor_update.c
@@ -0,0 +1,15 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+#include "../posix_pc_utils.h"
+
+void wuclass_temperature_humidity_sensor_setup(wuobject_t *wuobject) {}
+
+void wuclass_temperature_humidity_sensor_update(wuobject_t *wuobject) {
+	int temp_value = posix_property_get(wuobject, "temperature_sensor");
+	printf("WKPFUPDATE(TemperatureHumiditySensor): Sensed temperature value: %dC\n", temp_value);
+	wkpf_internal_write_property_int16(wuobject, WKPF_PROPERTY_TEMPERATURE_HUMIDITY_SENSOR_CURRENT_VALUE_TEMPERATURE, (int16_t)temp_value);
+
+	int hum_value = posix_property_get(wuobject, "humidity_sensor");
+	printf("WKPFUPDATE(TemperatureHumiditySensor): Sensed humidity value: %dC\n", hum_value);
+	wkpf_internal_write_property_int16(wuobject, WKPF_PROPERTY_TEMPERATURE_HUMIDITY_SENSOR_CURRENT_VALUE_HUMIDITY, (int16_t)hum_value);
+}
