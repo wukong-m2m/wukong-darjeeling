fad0266307eaeab6aaaee1b7660416747897165c
Niels Reijers <nielsreijers@gmail.com>
2014-02-04 11:48:56 +0800

    Issue #126 Changed Java network server and radio_networkserver.c to stop both from claiming 100% cpu in the busy loop. (bit of an ugly hack on the C side, but doing it properly would require too much refactoring at this point, and it should work well this way too.)

--- wukong/tools/wukong_network_server/WuKongNetworkServer.java
+++ wukong/tools/wukong_network_server/WuKongNetworkServer.java
@@ -6,7 +6,7 @@ import java.util.*;
 // s->c [42]
 // c->s 1 byte mode
 // if mode==MODE_MESSAGE
-//   c->s/s->c 1 byte length, 2 byte LE src 2 byte LE desr, length-5 bytes payload
+//   c->s/s->c 1 byte length, 2 byte LE src 2 byte LE dest, length-5 bytes payload
 // if mode==MODE_DISCOVERY
 //   s->c      2 byte length, (length-2/2) times 2 byte LE client id
 
@@ -15,8 +15,9 @@ public class WuKongNetworkServer extends Thread
 	protected static boolean serverContinue = true;
 	protected static Map<Integer, WuKongNetworkServer> clients;
 	protected Socket clientSocket;
+	protected BufferedInputStream in;
+	protected BufferedOutputStream out;
 	protected int clientId;
-	protected Queue<int[]> messages;
 	protected boolean keepRunning;
 
 	private final int MODE_MESSAGE = 1;
@@ -65,18 +66,29 @@ public class WuKongNetworkServer extends Thread
 
 	private WuKongNetworkServer (Socket clientSoc)
 	{
-		messages = new LinkedList<int[]>();
 		keepRunning = true;
 		clientSocket = clientSoc;
+		try {
+			clientSocket.setSoTimeout(1000);
+		}
+		catch (SocketException e) {
+			System.err.println("Could not set timeout for socket."); 
+			System.exit(1); 
+		}
+		try {
+			in = new BufferedInputStream(clientSocket.getInputStream());
+			out = new BufferedOutputStream(clientSocket.getOutputStream());
+		}
+		catch (IOException e) {
+			System.err.println("Could not create input/output streams."); 
+			System.exit(1); 
+		}
 		start();
 	}
 
 	public void run()
 	{
-		try { 
-			BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
-			BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
-
+		try {
 			// Say hi
 			out.write(42);
 			out.flush();
@@ -104,8 +116,6 @@ public class WuKongNetworkServer extends Thread
 	}
 
 	private void handle_mode_message(BufferedInputStream in, BufferedOutputStream out) throws IOException {
-		long lastHeartbeat = 0;
-
 		// Get client id
 		this.clientId = in.read();
 		if (this.clientId < 0)
@@ -120,51 +130,57 @@ public class WuKongNetworkServer extends Thread
 		WuKongNetworkServer.clients.put(this.clientId, this);
 
 		while(keepRunning) {
-			// Receive messages
-			if (in.available() > 0) {
-				int length = in.read();
-				int [] message = new int[length];
-				message[0] = length;
-				for (int i=1; i<length; i++)
-					message[i] = in.read();
-				int destId = message[3] + message[4]*256;
+			int length = 0;
 
-				System.out.print("Received message from " + this.clientId + ", for " + destId + ", length " + length);
-
-				WuKongNetworkServer destClient = WuKongNetworkServer.clients.get(destId);
-				if (destClient != null) {
-					destClient.messages.add(message);
-					System.out.println("");
-				}
-				else
-					System.out.println(" ---> dropped.");
+			try {
+				length = in.read();
 			}
-
-			// Send messages
-			if (this.messages.size() > 0) {
-				int [] message = this.messages.poll();
-				System.out.print("Forwarding message to node " + this.clientId);
-				for (int i=0; i<message[0]; i++) {
-					out.write(message[i]);
-					System.out.print(" [" + String.format("%02X ", message[i]) + "]");
-				}
-				out.flush();
-				System.out.println("");
-			}
-
-			// Heartbeat
-			if (System.currentTimeMillis() - lastHeartbeat > 1000) {
+			catch (SocketTimeoutException ste) {
+				// Nothing received from client for 1s. Send heartbeat to check if the connection still exists
 				try {
-					lastHeartbeat = System.currentTimeMillis();
 					out.write(0);
 					out.flush();
+					continue;
 				}
 				catch (IOException e){
 					this.keepRunning = false;
 				}
 			}
 
-			Thread.yield();
+			// Length byte received. Process the rest of the message
+			int [] message = new int[length];
+			message[0] = length;
+			for (int i=1; i<length; i++)
+				message[i] = in.read();
+			int destId = message[3] + message[4]*256;
+
+			System.out.print("Received message from " + this.clientId + ", for " + destId + ", length " + length);
+
+			WuKongNetworkServer destClient = WuKongNetworkServer.clients.get(destId);
+			if (destClient != null) {
+				System.out.println("");
+				destClient.sendMessage(message);
+			}
+			else {
+				System.out.println(" ---> dropped.");
+			}
+		}
+	}
+
+	private synchronized void sendMessage(int[] message) {
+		try {
+			System.out.print("Forwarding message to node " + this.clientId);
+			for (int i=0; i<message[0]; i++) {
+				out.write(message[i]);
+				System.out.print(" [" + String.format("%02X ", message[i]) + "]");
+			}
+			out.flush();
+			System.out.println("");
+		}
+		catch (IOException e) {
+			System.err.println("IOException sending message to node " + this.clientId); 
+			System.err.println(e);
+			this.keepRunning = false;
 		}
 	}
 
