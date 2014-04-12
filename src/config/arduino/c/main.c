1cdb8a71eb8c99a5ac273047559489acce10843e
wycc <wycca1@gmail.com>
2014-04-12 16:32:51 +0800

    Seperate the VM main loop as init and loop so that it can fit into the Arduino model

---
+++ src/config/arduino/c/main.c
@@ -0,0 +1,80 @@
+/*
+ * main.c
+ * 
+ * Copyright (c) 2008-2010 CSIRO, Delft University of Technology.
+ * 
+ * This file is part of Darjeeling.
+ * 
+ * Darjeeling is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Darjeeling is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Darjeeling.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "debug.h"
+#include "vm.h"
+#include "heap.h"
+#include "infusion.h"
+#include "types.h"
+#include "vmthread.h"
+#include "djtimer.h"
+#include "execution.h"
+#include "djarchive.h"
+#include "hooks.h"
+#include "core.h"
+
+#include "avr.h"
+#include "wkpf.h"
+#include "wkpf_wuobjects.h"
+#include "wkpf_links.h"
+
+
+extern const unsigned char di_lib_infusions_archive_data[];
+extern const unsigned char di_app_infusion_archive_data[];
+
+// From GENERATEDlibinit.c, which is generated during build based on the libraries in this config's libs.
+extern dj_named_native_handler java_library_native_handlers[];
+extern uint8_t java_library_native_handlers_length;
+
+
+unsigned char mem[HEAPSIZE];
+
+void wukong_setup()
+{
+	// Declared in djarchive.c so that the reprogramming code can find it.
+	di_app_archive = (dj_di_pointer)di_app_infusion_archive_data;
+
+	// initialise serial port
+	avr_serialInit(115200);
+
+	core_init(mem, HEAPSIZE);
+	dj_vm_main_init((dj_di_pointer)di_lib_infusions_archive_data, (dj_di_pointer)di_app_infusion_archive_data, java_library_native_handlers, java_library_native_handlers_length);
+}
+
+
+void wukong_loop()
+{
+	if  (dj_vm_loop()) {
+		wuobject_t *wuobject = NULL;
+		// Process any incoming messages
+		dj_hook_call(dj_core_pollingHook, NULL);
+		if (dj_exec_getRunlevel() == RUNLEVEL_RUNNING) {
+			// Propagate any dirty properties
+			wkpf_propagate_dirty_properties();
+			// Check if any wuobjects need updates
+			// Will call update() for native profiles directly,
+			// and return only true for virtual profiles requiring an update.
+			if(wkpf_get_next_wuobject_to_update(&wuobject)) {
+				return wuobject;
+			}
+		}
+	}
+}
