1dfec4f0f2e5bc8426da485b4aa17830e4a45634
chingchi <volvico@gmail.com>
2014-04-22 19:53:23 +0800

    Remove intermediate files

--- src/config/arduino/arduino/libraries/WukongVM/wkpf/wkpf_virtual_wuclasses.h
+++
@@ -1,8 +0,0 @@
-#ifndef WKPF_VIRTUAL_WUCLASSESH
-#define WKPF_VIRTUAL_WUCLASSESH
-
-#include "types.h"
-
-extern uint8_t wkpf_register_virtual_wuclass(uint16_t wuclass_id, update_function_t update, uint8_t number_of_properties, uint8_t properties[]);
-
-#endif // WKPF_VIRTUAL_WUCLASSESH
