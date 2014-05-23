wukong-m2m/wukong-darjeeling

commit d4ef1f077eb1c17fdc12891ec8066197907dce4e

Issue #151 Changed the way native methods are called. Instead of calling the generated handler function, callMethod now directly indexes the array of native handlers and calls the appropriate function. One reason for this change is that the infusion struct now contains a pointer to the array instead of to the handler function, so we can modify it for RTC compiled code.

src/core/c/types.h
@@ -88,12 +88,12 @@ __attribute__ ((__packed__))
 #endif
 ;
 
-typedef void (*dj_native_handler)(dj_global_id);
+typedef void (*native_method_function_t)(void);
 
 struct _dj_named_native_handler
 {
 	char * name;
-	dj_native_handler handler;
+	const native_method_function_t * handlers;
 };
 
 struct _dj_object
@@ -181,7 +181,7 @@ struct _dj_infusion
 	dj_di_pointer stringTable;
 
 	// pointer to native method handler
-	dj_native_handler native_handler;
+	const native_method_function_t * native_handlers;
 
 	// for dynamic adress translation
 	runtime_id_t class_base;

src/infuser/java/org/csiro/darjeeling/infuser/outputphase/CHeaderVisitor.java
@@ -74,7 +74,7 @@ public class CHeaderVisitor extends DescendingVisitor
 		writer.println("");
 		writer.println("#include \"types.h\"");
 		writer.println("");
-		writer.println(String.format("void %s_native_handler(dj_global_id id);", header.getInfusionName()));
+		writer.println(String.format("extern const native_method_function_t %s_native_method_function_pointers[];", header.getInfusionName()));
 		writer.println("");
 		
 		visit((ParentElement<Element>)element);

src/architecture/avr/c/program_mem.h
@@ -24,6 +24,9 @@
 
 #include <avr/pgmspace.h>
 
+// Macro to put things in program memory
+#define DJ_PROGMEM __flash
+
 typedef unsigned int dj_di_pointer;
 
 extern char DJ_SYSTEM_INFUSION_NAME[];

src/architecture/native/c/program_mem.h
@@ -24,6 +24,9 @@
 
 #include <stdint.h>
 
+// Macro to put things in program memory
+#define DJ_PROGMEM 
+
 typedef unsigned long dj_di_pointer;
 
 #define DJ_DI_NOT_SET -1

src/infuser/java/org/csiro/darjeeling/infuser/outputphase/CFileVisitor.java
@@ -60,8 +60,7 @@ public class CFileVisitor extends DescendingVisitor
 
 		// write the array of method pointers
 		String arrayname = String.format("%s_native_method_function_pointers", element.getHeader().getInfusionName());
-		writer.println("typedef void (*native_method_function_t)(void);");
-		writer.println(String.format("native_method_function_t %s[%d] = {",
+		writer.println(String.format("const DJ_PROGMEM native_method_function_t %s[%d] = {",
 									 arrayname,
 									 methodImplementations.size()));
 		int checkId = 0;		
@@ -83,14 +82,6 @@ public class CFileVisitor extends DescendingVisitor
 			}
 		}
 		writer.println("};");
-
-
-		// generate handler code
-		writer.println(String.format("void %s_native_handler(dj_global_id id)", element.getHeader().getInfusionName()));
-		writer.println("{");
-		writer.println(String.format("\t%s[id.entity_id]();", arrayname));
-		writer.println("}");
-		writer.println("");
 	}
 	
 	@Override

src/infuser/java/org/csiro/darjeeling/libinit/ant/LibInitTask.java
@@ -81,7 +81,7 @@ public class LibInitTask extends Task
 
 			fout.println("dj_named_native_handler java_library_native_handlers[] = {");
 			for (String library : javaLibrariesArray) {
-				fout.println("\t{ \"" + library + "\", &" + library + "_native_handler },");
+				fout.println("\t{ \"" + library + "\", " + library + "_native_method_function_pointers },");
 			}
 			fout.println("};");
 			fout.println("uint8_t java_library_native_handlers_length = " + javaLibrariesArray.size() + ";");

src/lib/vm/c/common/execution.c
@@ -830,10 +830,10 @@ static inline void dj_exec_passParameters(dj_frame *frame, dj_global_id methodIm
 static inline void callMethod(dj_global_id methodImplId, int virtualCall)
 {
 	dj_frame *frame;
-	dj_native_handler handler;
 	bool isReturnReference=false;
 	int oldNumRefStack, numRefStack;
 	int diffRefArgs;
+	const native_method_function_t *handlers = methodImplId.infusion->native_handlers;
 
 
 #ifdef DARJEELING_DEBUG
@@ -859,9 +859,10 @@ static inline void callMethod(dj_global_id methodImplId, int virtualCall)
 		DEBUG_LOG(DBG_DARJEELING, "Invoking native method ... \n");
 #endif
 
+		native_method_function_t handler = (handlers != NULL ? methodImplId.infusion->native_handlers[methodImplId.entity_id] : NULL);
+
 		// the method is native, check if we have a native handler
 		// for the infusion the method is in
-		handler = methodImplId.infusion->native_handler;
 		if (handler != NULL)
 		{
 			// Observe the number of reference elements on the ref. stack:
@@ -869,7 +870,7 @@ static inline void callMethod(dj_global_id methodImplId, int virtualCall)
 			oldNumRefStack = (ref_t *)dj_frame_getStackEnd(frame) - refStack;
 
 			// execute the method by calling the infusion's native handler
-			handler(methodImplId);
+			handler();
 
 			// The reference stack needs right treatment now, so remember the
 			// contract for parameters and return values of native methods:

src/lib/vm/c/common/infusion.c
@@ -68,7 +68,7 @@ dj_infusion *dj_infusion_create(dj_di_pointer staticFieldInfo, int nrImportedInf
 	ret->classList = 0;
 	ret->methodImplementationList = 0;
 	ret->next = 0;
-	ret->native_handler = 0;
+	ret->native_handlers = 0;
 
 	// initialise to DJ_DI_NOT_SET
 	ret->header = DJ_DI_NOT_SET;

commit ed24ceb4d077fc73214eb26164aabeb639486f42

Issue #151 Fixed bug in calling native methods introduced in d4ef1f077eb1c17fdc12891ec8066197907dce4e. (worked on pc, but not on avr)

src/core/c/types.h
@@ -93,7 +93,7 @@ typedef void (*native_method_function_t)(void);
 struct _dj_named_native_handler
 {
 	char * name;
-	const native_method_function_t * handlers;
+	const DJ_PROGMEM native_method_function_t * handlers;
 };
 
 struct _dj_object
@@ -181,7 +181,7 @@ struct _dj_infusion
 	dj_di_pointer stringTable;
 
 	// pointer to native method handler
-	const native_method_function_t * native_handlers;
+	const DJ_PROGMEM native_method_function_t * native_handlers;
 
 	// for dynamic adress translation
 	runtime_id_t class_base;

src/infuser/java/org/csiro/darjeeling/infuser/outputphase/CHeaderVisitor.java
@@ -74,7 +74,7 @@ public class CHeaderVisitor extends DescendingVisitor
 		writer.println("");
 		writer.println("#include \"types.h\"");
 		writer.println("");
-		writer.println(String.format("extern const native_method_function_t %s_native_method_function_pointers[];", header.getInfusionName()));
+		writer.println(String.format("extern const DJ_PROGMEM native_method_function_t %s_native_method_function_pointers[];", header.getInfusionName()));
 		writer.println("");
 		
 		visit((ParentElement<Element>)element);