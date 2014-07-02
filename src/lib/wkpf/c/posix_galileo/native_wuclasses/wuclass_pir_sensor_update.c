3f2a119359d30b970b1f44565b87090df54bf0ce
B.L.Tsai <b95008@csie.ntu.edu.tw>
2014-07-02 21:35:24 +0800

    Add PIR into galileo. Add slider into wunode. Add Pressure Sensor 0 into native

---
+++ src/lib/wkpf/c/posix_galileo/native_wuclasses/wuclass_pir_sensor_update.c
@@ -0,0 +1,25 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void wuclass_pir_sensor_setup(wuobject_t *wuobject) {
+    if( access( "/sys/class/gpio/gpio27/value", F_OK ) == -1 ) {
+        system("echo -n 27 > /sys/class/gpio/export");
+    }
+    system("echo -n in > /sys/class/gpio/gpio27/direction"); //gpio27-pin7, gpio26-pin8
+}
+
+void wuclass_pir_sensor_update(wuobject_t *wuobject) {
+    bool value;
+    int value_i;
+    FILE *fp;
+    fp = fopen("/sys/class/gpio/gpio27/value", "r");
+    fscanf(fp, "%d", &value_i);
+    value = (value_i != 0);
+    printf("WKPFUPDATE(PirSensor): Sensed binary value: %d\n", value);
+    //system("echo \"value\" > /dev/ttyGS0"); //Serial (IDE Serial Monitor)
+    //delay(500);
+    wkpf_internal_write_property_boolean(wuobject, WKPF_PROPERTY_PIR_SENSOR_CURRENT_VALUE, value);
+}
