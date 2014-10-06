0e5c4544949727aab5bb1d1166ff98efa24e98f1
catlikethief <a50119729@gmail.com>
2014-10-06 16:13:07 +0800

    add discovery unit test.

---
+++ wukong/master/tests/unit_tests/test_device/test_discovery.py
@@ -0,0 +1,25 @@
+import os, sys
+import unittest
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
+from test_environment_device import WuTest
+from configuration import *
+
+class TestDiscovery(unittest.TestCase):
+
+    def setUp(self):
+    	self.test = WuTest()
+
+    def test_basic_discoveryAPI(self):
+    	self.test.discovery()
+        res = self.test.countWuObjectByWuClassID(204)
+        self.assertEqual(res, 1)
+
+        res = self.test.countWuObjectByWuClassID(1000)
+        self.assertEqual(res, 0)
+
+        res = self.test.countWuObjectByWuClassID(2000)
+        self.assertEqual(res, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
