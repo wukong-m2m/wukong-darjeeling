cdf631cf33fe308db38e51e2b2dfdad4858b870b
catlikethief <a50119729@gmail.com>
2014-10-06 16:20:42 +0800

    Add get/set property unit test.

---
+++ wukong/master/tests/unit_tests/test_device/test_property.py
@@ -0,0 +1,47 @@
+import os, sys
+import time
+import unittest
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
+from test_environment_device import WuTest
+from configuration import *
+
+
+class TestProperty(unittest.TestCase):
+
+    def setUp(self):
+        self.test = WuTest()
+
+    def test_basic_propertyAPI(self):
+        node_id = 2
+
+        port = 1
+        wuclassID = 204
+
+        property_number = 0
+        ans = 0
+        self.test.setProperty(node_id, port, wuclassID, property_number, 1, ans)
+        res = self.test.getProperty(node_id, port, wuclassID, property_number)
+
+        ans = 1
+        self.test.setProperty(node_id, port, wuclassID, property_number, 1, ans)
+        res = self.test.getProperty(node_id, port, wuclassID, property_number)
+        self.assertEqual(res[0], ans)
+
+        property_number = 1
+        ans = 1
+        self.test.setProperty(node_id, port, wuclassID, property_number, 0, ans)
+        res = self.test.getProperty(node_id, port, wuclassID, property_number)
+        self.assertEqual(res[0], ans)
+
+        ans = 4
+        self.test.setProperty(node_id, port, wuclassID, property_number, 0, ans)
+        res = self.test.getProperty(node_id, port, wuclassID, property_number)
+        self.assertEqual(res[0], ans)
+
+        ans = 100
+        self.test.setProperty(node_id, port, wuclassID, property_number, 0, ans)
+        res = self.test.getProperty(node_id, port, wuclassID, property_number)
+        self.assertEqual(res[0], ans)
+
+if __name__ == '__main__':
+    unittest.main()
