0219021266136d90582bfc0eea29870c4be3029e
catlikethief <a50119729@gmail.com>
2014-10-06 17:46:40 +0800

    Add test script to run all tests and generate HTML report.

---
+++ wukong/master/tests/test_devices.py
@@ -0,0 +1,28 @@
+import datetime
+from unittest import TestLoader, TestSuite
+from HTMLTestRunner import HTMLTestRunner
+import os,sys,time
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unit_tests/test_device'))
+
+from test_location import TestLocation
+from test_property import TestProperty
+from test_discovery import TestDiscovery
+
+
+if __name__ == '__main__':
+	filename = datetime.datetime.now().strftime("report.html")
+	date = datetime.datetime.now().strftime("%Y_%m_%d_%H%M")
+
+	try:
+		os.mkdir('reports/%s' % (date))
+		output = open('reports/%s/%s' % (date, filename), "wb")
+		loader = TestLoader()
+		suite = TestSuite((
+				loader.loadTestsFromTestCase(TestLocation),
+				loader.loadTestsFromTestCase(TestProperty),
+				loader.loadTestsFromTestCase(TestDiscovery)
+		))
+		runner = HTMLTestRunner(stream=output, verbosity=1, title="WuKong Testing")
+		runner.run(suite)
+	except:
+		pass
