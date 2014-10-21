7b99899ca0ea41885ccdb4f694c701d5e69dcfa1
chingchi <chingchichuang1015@gmail.com>
2014-10-21 17:19:33 +0800

    Fix the error in test_property.py

--- wukong/master/tests/unit_tests/test_device/test_property.py
+++ wukong/master/tests/unit_tests/test_device/test_property.py
@@ -13,26 +13,33 @@ class TestProperty(unittest.TestCase):
 
     def test_basic_propertyAPI(self):
         node_id = 2
-        port = 1
+        port = 3
         wuclassID = 204
         property_number = 1
         datatype = 'short' # boolean, short, refresh_rate
         
         ans = random.randint(0, 255)
         self.test.setProperty(node_id, port, wuclassID, property_number, datatype, ans)
+        time.sleep(SLEEP_SECS)
         res = self.test.getProperty(node_id, port, wuclassID, property_number)
 
+        self.assertEqual(res, ans)
+
+    
     def test_strength_propertyAPI(self):
         for i in xrange(TEST_PROPERTY_STRENGTH_NUMBER):
             node_id = 2
-            port = 1
+            port = 3
             wuclassID = 204
             property_number = 1
             datatype = 'short'
             
             ans = random.randint(0, 255)      
             self.test.setProperty(node_id, port, wuclassID, property_number, datatype, ans)
+            time.sleep(SLEEP_SECS)
             res = self.test.getProperty(node_id, port, wuclassID, property_number)
+
+            self.assertEqual(res, ans)
 
 if __name__ == '__main__':
     unittest.main()
