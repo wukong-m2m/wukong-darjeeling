506a238a27e3f8a05e49c334437aa5ddcd21cccf
Niels Reijers <nielsreijers@gmail.com>
2014-11-06 16:50:02 +0800

    Issue #166 Renamed dummy wuclass for Galileo to "sound".

--- src/lib/wkpf/c/posix_galileo/native_wuclasses/wuclass_dummy_sound_actuator_update.c
+++ src/lib/wkpf/c/posix_galileo/native_wuclasses/wuclass_dummy_sound_actuator_update.c
@@ -3,8 +3,8 @@
 
 // No implementation yet. Just here for the test framework
 
-void wuclass_sound_sensor_setup(wuobject_t *wuobject) {
+void wuclass_sound_setup(wuobject_t *wuobject) {
 }
 
-void wuclass_sound_sensor_update(wuobject_t *wuobject) {
+void wuclass_sound_update(wuobject_t *wuobject) {
 }
