dce377a193287d7eae1fe9c70a2dee232a98a0fc
Niels Reijers <nielsreijers@gmail.com>
2015-02-13 14:57:42 +0800

    Issue #151 Another small optimisation to the Java code. Reduces bubble sort time from 7.9M to 7.3M cycles. 7.6x slowdown.

--- src/lib/rtctest1/java/javax/rtctest/RTCTestBubbleSort.java
+++ src/lib/rtctest1/java/javax/rtctest/RTCTestBubbleSort.java
@@ -24,14 +24,15 @@ public class RTCTestBubbleSort {
 
 		for (int i=0; i<NUMNUMBERS; i++) {
 			int x=(NUMNUMBERS-i-1); // This doesn't get optimised the way I expected it would. Without this extra variable, it will calculate NUMNUMBERS-i-1 on each interation of the inner loop! (speedup 14.7M -> 14.2M cycles)
+			int j_plus_one = 1; // Same goes for "j+1"
 			for (int j=0; j<x; j++) {
-				int j_plus_one = j+1; // Same goes for "j+1"
 				short val_at_j = numbers[j];
 				short val_at_j_plus_one = numbers[j_plus_one];
 				if (val_at_j>val_at_j_plus_one) {
 					numbers[j] = val_at_j_plus_one;
 					numbers[j_plus_one] = val_at_j;
 				}
+				j_plus_one++;
 			}
 		}
 
