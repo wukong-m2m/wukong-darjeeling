4a8a6baf5bebfec8cd3abdf4ee7ab7ec6668444c
Niels Reijers <nielsreijers@gmail.com>
2015-05-07 12:20:06 +0800

    Issue #168 Collect library infusions in a DJ archive, convert it to a c array, and include it in the VM.

--- src/config/avrora/c/tmpMain.c
+++ src/config/avrora/c/tmpMain.c
@@ -4,7 +4,6 @@
 #include "types.h"
 
 int main() { return 0; }
-const unsigned char di_lib_infusions_archive_data[0];
 const unsigned char di_app_infusion_archive_data[0];
 void rtc_compile_lib(dj_infusion *infusion) {}
 
