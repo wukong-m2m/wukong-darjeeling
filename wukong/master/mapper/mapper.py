03cd46b73031c88547798c48c0e930756f5e9b8d
Flint <zzk012@gmail.com>
2015-07-25 15:08:02 +0800

    change mapper.py

--- wukong/master/mapper/mapper.py
+++ wukong/master/mapper/mapper.py
@@ -23,6 +23,9 @@ from subprocess import Popen, PIPE, STDOUT
 
 from configuration import *
 from wkpf.globals import *
+import pickle
+
+ChangeSets = namedtuple('ChangeSets', ['components', 'links', 'heartbeatgroups'])
 
 # allcandidates are all node ids ([int])
 def constructHeartbeatGroups(heartbeatgroups, routingTable, allcandidates):
@@ -80,22 +83,22 @@ def sortCandidates(wuObjects):
 #    )], links=[], heartbeatgroups=[])
 #############################
 
-def firstCandidate(logger, changesets, routingTable, locTree):
+def firstCandidate(logger, changesets, routingTable, locTree, flag = None):
     set_wukong_status('Mapping')
     logger.clearMappingStatus() # clear previous mapping status
 
     #input: nodes, WuObjects, WuLinks, WuClassDefsm, wuObjects is a list of wuobject list corresponding to group mapping
     #output: assign node id to WuObjects
     # TODO: mapping results for generating the appropriate instiantiation for different nodes
-
     mapping_result = True
     #clear all "mapped" tags in every node before mapping
-    for nodeid in WuNode.node_dict:
-        node = locTree.getNodeInfoById(nodeid)
-        if node != None:
-            for wuobj in node.wuobjects.values():
-                wuobj.mapped = False
-            
+    if flag == None:
+        for nodeid in WuNode.node_dict:
+            node = locTree.getNodeInfoById(nodeid)
+            if node != None:
+                for wuobj in node.wuobjects.values():
+                    wuobj.mapped = False
+
     # construct and filter candidates for every component on the FBP (could be the same wuclass but with different policy)
     for component in changesets.components:
         # filter by location
@@ -104,7 +107,7 @@ def firstCandidate(logger, changesets, routingTable, locTree):
         print component
         try:
             candidates, rating = locParser.parse(component.location)
-            print "candidates", candidates, rating
+            print "candidates: ", candidates, "rating: ", rating
         except:
             #no mapping result
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -125,6 +128,7 @@ def firstCandidate(logger, changesets, routingTable, locTree):
             has_wuclass = wuclassdef.id in node.wuclasses.keys()
             wuobj_found = False
             for avail_wuobj in available_wuobjects:
+                print avail_wuobj , "avail_wuobj"
                 # use existing native wuobject, caution given to obj mapped due to previous candidates
                 if not avail_wuobj.virtual and not avail_wuobj.mapped:
                     print avail_wuobj.virtual, avail_wuobj.mapped
@@ -204,4 +208,179 @@ def firstCandidate(logger, changesets, routingTable, locTree):
             #senNd.temp_port_list = []
 
     set_wukong_status('')
+    print "mapping_result: ",mapping_result
+
+    if flag == None:
+        save_map("changesets.tmp",changesets)
+        #print changesets.links[0].from_component.location
+        #print changesets.links[0].from_property_name, " ", changesets.links[0].from_property.name
+        #print changesets.links[0].to_component.location, " " , changesets.links[0].to_property_name, " " , changesets.links[0].to_property 
+
     return mapping_result
+
+def Compare_changesets (new_changesets, old_changesets):
+    diff = ChangeSets(components = [], links = [], heartbeatgroups = [])
+    same = ChangeSets(components = [], links = [], heartbeatgroups = [])
+    tmp = [] #compoents will be used
+    conflict = False
+
+    for new_l in new_changesets.links:
+        flag = 0
+        for old_l in old_changesets.links:
+            if new_l.from_component.location == old_l.from_component.location and new_l.to_component.location == old_l.to_component.location and new_l.from_component.type == old_l.from_component.type and new_l.to_component.type == old_l.to_component.type:  
+                same.links.append(new_l)
+                flag = 1
+        if flag == 0:
+            if new_l.from_component.location not in tmp:
+                tmp.append((new_l.from_component.location,new_l.from_component.type))
+            if new_l.to_component.location not in tmp:
+                tmp.append((new_l.to_component.location,new_l.to_component.type))
+            diff.links.append(new_l)
+
+    print "objects will be used:", tmp
+    for new_c in new_changesets.components:
+        flag = 0
+        for values in tmp:
+            if new_c.location == values[0] and new_c.type == values[1]:
+                diff.components.append(new_c)
+                flag = 1
+                break
+            elif new_c.type == "Server":
+                diff.components.append(new_c)
+                flag = 1
+                break
+        if flag == 0:
+            for old_c in old_changesets.components:
+                if new_c.location == old_c.location and new_c.type == old_c.type:
+                    same.components.append(new_c)
+                    flag = 1
+                    break
+            if flag == 0:
+                diff.components.append(new_c)
+    #check conflict
+    for old_l in old_changesets.links:
+        if (old_l.from_component.location, old_l.from_component.type) in tmp or (old_l.to_component.location, old_l.to_component.type) in tmp:
+            #share policy have bugs
+            if share_policy().pessimistic(old_l):
+                diff.links.append(old_l)
+            else:
+                conflict = True
+                break
+
+    extra_component = []
+    extra_links = []
+    for values in tmp:	
+        for old_l in old_changesets.links:
+            if old_l.from_component.location == values[0] or old_l.to_component.location == values[0]:
+                if old_l not in extra_links:
+                    extra_links.append(old_l)
+        for old_c in old_changesets.components:
+            if old_c.location == values[0]:
+                if old_c.location not in extra_component:
+                    extra_component.append(old_c)
+    #for tmp2 in diff.links:
+    #    print "diff from:",tmp2.from_component.location,"to:",tmp2.to_component.location
+
+    return (same,diff,extra_component,extra_links,conflict)
+
+def least_changed(logger, changesets, routingTable, locTree):
+    try:
+        past_changesets = load_map("changesets.tmp")
+        past_changesets = unintstall(past_changesets, locTree)
+    except:
+        mapping_result = firstCandidate(logger, changesets, routingTable, locTree)
+        return mapping_result
+    same, diff, extra_component, extra_links,conflict = Compare_changesets(changesets, past_changesets) 
+    print "same: ", same, "\ndiff: ", diff
+    if diff.components != []:
+        if conflict:
+            mapping_result = firstCandidate(logger, changesets, routingTable, locTree)
+        else:
+            mapping_result = firstCandidate(logger, diff, routingTable, locTree, 1)
+            if extra_component != []:
+                for tmp in extra_component:
+                    changesets.components.append(tmp)
+            if extra_links != []:
+                for tmp in extra_links:
+                    changesets.links.append(tmp)
+            dump_changesets(changesets)
+            #same = diff + same
+            for tmp in diff.components:
+                same.components.append(tmp)
+            for tmp in diff.links:
+                if tmp not in same.links:
+                    same.links.append(tmp)
+            for tmp in diff.heartbeatgroups:
+                same.heartbeatgroups.append(tmp)
+            save_map("changesets.tmp",same)
+    else :
+        mapping_result = False
+        print "no differentce"
+    return mapping_result
+
+#return True if object can be shared 
+class share_policy(object):
+    def __init__(self):
+        pass
+#        self._unused_wuobj = {}
+#        self._used_wuobj = {}
+#        i = 1
+#        j = 1
+#        for nodeid in WuNode.node_dict:
+#            node = locTree.getNodeInfoById(nodeid)
+#            if node != None:
+#                for wuobj in node.wuobjects.values():
+#                    if wuobj.mapped == True:
+#                        self._used_wuobj.update({i:wuobj})
+#                        i +=1
+#                    else:
+#                        self._unused_wuobj.update({j:wuobj})
+#                        j +=1
+
+    def pessimistic(self,link): #only readonly components can be shared
+        if link.from_property.access == "readonly":
+            return True
+        elif link.to_component.type == "Server":
+            return True
+        else:
+            return False
+
+def unintstall(changesets,locTree):
+    #To do:change xml.state to unused
+    node_id_list = []
+    for node_id in WuNode.node_dict:
+        node_id_list.append(node_id)
+    print "node id list:" ,node_id_list
+    for component in changesets.components:
+        if component.instances[0].wunode.id not in node_id_list:
+            changesets.components.remove(component)
+    return changesets
+
+def save_map(name,msg):
+    with open(name,"wb") as f:
+        pickle.dump(msg,f, pickle.HIGHEST_PROTOCOL)
+def load_map(name):
+    with open(name,"rb") as f:
+        return pickle.load(f)
+
+def dump_changesets(changesets):
+    for component in changesets.components:
+        print "location: ",component.location, "type:", component.type
+    for link in changesets.links:
+        print "from location:" , link.from_component.location, "to location:", link.to_component.location
+        print "from type:", link.from_component.type, "to type:", link.to_component.type
+
+def delete_application(logger, changesets, routingTable, locTree):
+    import os
+    try:
+        changesets = load_map("changesets.tmp")
+        changesets = unintstall(changesets, locTree)
+    except:
+        print "file does not exist"
+    app_dir = changesets.component.application_hashed_name
+    print "delete: " ,app_dir
+    os.shutil.retree("../apps/" + app_dir)
+
+
+
+
