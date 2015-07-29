a4ec0389780aa06739f956189d09feb6c3c32a0a
fakewen <fakewen@gmail.com>
2015-07-29 22:19:47 +0800

    user aware wuclass debug

---
+++ src/lib/wkpf/c/posix.galileo/native_wuclasses/wuclass_user_id_update.c
@@ -0,0 +1,71 @@
+#include "debug.h"
+#include "native_wuclasses.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+#include <math.h>
+#include "config.h"
+#include "mraa.h"
+
+mraa_uart_context user_uart;
+int user_ttyFd;
+
+bool wuclass_user_id_dataAvailable(unsigned int millis){
+  if (user_ttyFd == -1){return false;}
+  struct timeval timeout;
+  // no waiting
+  timeout.tv_sec = 0;
+  timeout.tv_usec = millis * 1000;
+  // int nfds; //unknown
+  fd_set readfds;
+  FD_ZERO(&readfds);
+  FD_SET(user_ttyFd, &readfds);
+  if (select(user_ttyFd + 1, &readfds, NULL, NULL, &timeout) > 0)
+  {
+    return true;                // data is ready
+  }else{
+    return false;
+  }
+}
+
+
+void wuclass_user_id_setup(wuobject_t *wuobject) {
+  user_ttyFd = -1;
+  if ( !(user_uart = mraa_uart_init(0)) ){
+    DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): uart failure\n");                
+    return;
+  }
+  const char *devPath = mraa_uart_get_dev_path(user_uart);
+
+  if (!devPath){
+    DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): uart failure\n");                
+    return;
+  }
+  if ( (user_ttyFd = open(devPath, O_RDWR)) == -1){
+    return;
+  }
+}
+
+void wuclass_user_id_update(wuobject_t *wuobject) {
+    char buffer_rev[30]={};
+    bool available = wuclass_user_id_dataAvailable(1000);
+    DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): available: %d\n", available);                
+    if(available){                                       
+        int x = read(user_ttyFd, buffer_rev, 30);
+        if (x < 0) {
+            DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): uart error\n");
+            return; 
+        }
+        DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): size: %d buffer: %s\n",x , buffer_rev); 
+        int16_t output = (int16_t)atoi(buffer_rev);
+        wkpf_internal_write_property_int16(wuobject, WKPF_PROPERTY_USER_ID_USER_ID, output);
+        DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(userID): output %d\n", output);
+    }
+}
