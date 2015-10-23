30e137f171552cc1799b7c475f2053ff46269277
B.L.Tsai <b95008@csie.ntu.edu.tw>
2015-10-23 22:58:35 +0800

    Add Ultrasound wuclass to wudevice; Update arduino IR control bridge and its library

--- wukong/tools/arduino/Arduino_IR_Control_TV/Arduino_IR_Control_TV.ino
+++ wukong/tools/arduino/Arduino_IR_Control_TV/Arduino_IR_Control_TV.ino
@@ -1,5 +1,6 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
+#include <stdlib.h>
 
 #define PanasonicAddress      0x4004     // Panasonic address (Pre data) 
 #define PanasonicPower        0x100BCBD  // Panasonic Power button
@@ -12,24 +13,39 @@
 
 #define TV_ON_SPAN_MS 5000
 #define TV_OFF_SPAN_MS 1000
-#define TV_REQUIRED_CHECK_COUNT 1
-#define TV_VOLUME_PUSH_DELAY 10
-#define TV_VOLUME_DOWN_PUSH 100
-#define TV_VOLUME_UP_PUSH 50
+#define TV_REQUIRED_CHECK_COUNT 0
+#define TV_VOLUME_PUSH_DELAY 80
+#define TV_VOLUME_DOWN_PUSH 75
+#define TV_VOLUME_UP_PUSH 50 + 1
 
 #define TV_ON_INTERRUPT_STATE HIGH
 #define TV_MUTE_INTERRUPT_STATE HIGH
 
+#define LED_ON LOW
+#define LED_OFF HIGH
+
 IRsend irsend;
 
-int tv_on_off_interrupt_pin = 3;
-int tv_mute_interrupt_pin = 4;
-int tv_on_off_output_pin = 5;
+int tv_on_off_output_pin = 8;
+int tv_on_off_interrupt_pin = 9;
+int tv_mute_interrupt_pin = 10;
 
 int tv_current_status_led_pin = A8;
 int tv_on_off_interrupt_led_pin = A9;
 int tv_check_count_led_pin = A10;
 int tv_mute_interrupt_led_pin = A11;
+
+#define PEAK_TO_PEAK_MEDIAN 12
+#define WINDOW_LENGTH 24
+#define EXP_AVG_ALPHA 0.03
+#define EXP_AVG_THRESHOLD 6.5
+#define SENSE_SPEED_MS 16
+
+int tv_current_sensor_buf[WINDOW_LENGTH];
+bool tv_current_status = false;
+int ring_index = 0;
+double accum_value = -1.0;
+unsigned long last_tv_current_read_time = 0;
 
 bool last_tv_on_off_interrupt = false;
 unsigned long last_tv_interrupt_time;
@@ -37,23 +53,70 @@ int check_tv_count = 0;
 bool last_tv_mute_interrupt = false;
 
 
-bool read_tv_current_status(){
-   int v;
-   int instant_max = 0;
-   int count;
-   for (count = 0; count < 20; count++){
-     v = abs(analogRead(A13) - 512);
-     if (v > instant_max && v < SPURIOUS)
-     {
-       instant_max = v;
-     }
-     delay(50);
-   }
-   if (instant_max > THESHOLD){
-     return true;
-   } else {
-     return false;
-   }
+void read_tv_current_status(){
+//   int v;
+//   int instant_max = 0;
+//   int count;
+//   for (count = 0; count < 20; count++){
+//     v = abs(analogRead(A13) - 512);
+//     if (v > instant_max && v < SPURIOUS)
+//     {
+//       instant_max = v;
+//     }
+//     delay(50);
+//   }
+//   if (instant_max > THESHOLD){
+//     return true;
+//   } else {
+//     return false;
+//   }
+  // Algorithm 2
+  unsigned long current_read_time = millis();
+  if (current_read_time - last_tv_current_read_time < SENSE_SPEED_MS)
+  {
+    return;
+  }
+  else
+  {
+    last_tv_current_read_time = current_read_time;
+  }   
+  
+  int delta_value, read_value;
+  read_value = analogRead(A13);
+  if (ring_index < WINDOW_LENGTH)
+  {
+    tv_current_sensor_buf[ring_index] = read_value;
+    if (ring_index >= PEAK_TO_PEAK_MEDIAN)
+    {
+      delta_value = (read_value - tv_current_sensor_buf[ring_index-PEAK_TO_PEAK_MEDIAN])/PEAK_TO_PEAK_MEDIAN;
+      delta_value = delta_value * delta_value;
+      tv_current_sensor_buf[ring_index-PEAK_TO_PEAK_MEDIAN] = read_value;
+      if (accum_value >= 0)
+      {
+        accum_value = EXP_AVG_ALPHA * delta_value + (1-EXP_AVG_ALPHA) * accum_value;
+        if (accum_value > EXP_AVG_THRESHOLD && !tv_current_status)
+        {
+          tv_current_status = true; 
+          Serial.print(F("Detected: TV is on\n"));
+        }
+        else if(accum_value <= EXP_AVG_THRESHOLD && tv_current_status)
+        {
+          tv_current_status = false;
+          Serial.print(F("Detected: TV is off\n"));
+        }
+      }
+      else
+      {
+        accum_value = delta_value;
+      }
+    }
+  }
+  ring_index++;
+  if (ring_index == WINDOW_LENGTH)
+  {
+    ring_index = PEAK_TO_PEAK_MEDIAN;
+  }
+  
 }
 
 void setupIRLED()
@@ -71,37 +134,42 @@ void setupIRLED()
   PORTE &= ~(1<<2);
 }
 
-
 void checkIfTVOnOffTriggered(){
+
+  read_tv_current_status();
+  digitalWrite(tv_on_off_output_pin, tv_current_status);
+  if (tv_current_status)
+  {
+    digitalWrite(tv_current_status_led_pin, LED_ON);
+//    Serial.print(F("Detected: TV is on\n"));
+  }
+  else
+  {
+    digitalWrite(tv_current_status_led_pin, LED_OFF);
+//    Serial.print(F("Detected: TV is off\n"));
+  }
+  
+  
   if (digitalRead(tv_on_off_interrupt_pin) == TV_ON_INTERRUPT_STATE && !last_tv_on_off_interrupt)
   {
     last_tv_on_off_interrupt = true;
     last_tv_interrupt_time = millis();
-    irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
-    analogWrite(tv_on_off_interrupt_led_pin, 255);
+    if(!tv_current_status) irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
+    digitalWrite(tv_on_off_interrupt_led_pin, LED_ON);
+    Serial.print(F("Interrupt: Turn ON TV\n"));
     check_tv_count = TV_REQUIRED_CHECK_COUNT; // to remind system to examine whether TV is truly on
   }
   else if (digitalRead(tv_on_off_interrupt_pin) != TV_ON_INTERRUPT_STATE && last_tv_on_off_interrupt)
   {
     last_tv_on_off_interrupt = false;
     last_tv_interrupt_time = millis();
-    irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
-    analogWrite(tv_on_off_interrupt_led_pin, 0);
+    if(tv_current_status) irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
+    digitalWrite(tv_on_off_interrupt_led_pin, LED_OFF);
+    Serial.print(F("Interrupt: Turn OFF TV\n"));
     check_tv_count = TV_REQUIRED_CHECK_COUNT; // to remind system to examine whether TV is truly off
   }
   if (check_tv_count) 
-    digitalWrite(tv_check_count_led_pin, HIGH);
-  
-  bool tv_current_status = read_tv_current_status();
-  digitalWrite(tv_on_off_output_pin, tv_current_status);
-  if (tv_current_status)
-  {
-    analogWrite(tv_current_status_led_pin, 255);
-  }
-  else
-  {
-    analogWrite(tv_current_status_led_pin, 0);
-  }
+    digitalWrite(tv_check_count_led_pin, LED_ON);
   
   // Examination of TV status once after the interrupt signal
   if(check_tv_count > 0)
@@ -112,13 +180,14 @@ void checkIfTVOnOffTriggered(){
       if (!tv_current_status)
       {
         irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
+        Serial.print(F("Retry: Turn ON TV\n"));
         last_tv_interrupt_time = millis();
         check_tv_count--;
       }
       else
       {
         check_tv_count = 0;
-        digitalWrite(tv_check_count_led_pin, LOW);
+        digitalWrite(tv_check_count_led_pin, LED_ON);
       }
     }
     else if (!last_tv_on_off_interrupt && (millis() - last_tv_interrupt_time > TV_OFF_SPAN_MS))
@@ -126,30 +195,34 @@ void checkIfTVOnOffTriggered(){
       if (tv_current_status)
       {
         irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
+        Serial.print(F("Retry: Turn OFF TV\n"));
         last_tv_interrupt_time = millis();
         check_tv_count--;
       }
       else
       {
         check_tv_count = 0;
-        digitalWrite(tv_check_count_led_pin, LOW);
+        digitalWrite(tv_check_count_led_pin, LED_OFF);
       }
     }
   }
 }
 
 void checkIfTVMuteTriggered(){
+  if (!tv_current_status) return;
+  
   if (digitalRead(tv_mute_interrupt_pin) == TV_MUTE_INTERRUPT_STATE && !last_tv_mute_interrupt)
   {
     last_tv_mute_interrupt = true;
-  
+
     for (int i = 0; i < TV_VOLUME_DOWN_PUSH; ++i){
       irsend.sendPanasonic(PanasonicAddress, PanasonicVolumeDown);
       delay(TV_VOLUME_PUSH_DELAY);
     }
     irsend.sendPanasonic(PanasonicAddress, PanasonicMute);
+    Serial.print(F("Interrupt: Mute TV\n"));
 
-    analogWrite(tv_mute_interrupt_led_pin, 255);
+    digitalWrite(tv_mute_interrupt_led_pin, LED_ON);
   }
   else if (digitalRead(tv_mute_interrupt_pin) != TV_MUTE_INTERRUPT_STATE && last_tv_mute_interrupt)
   {
@@ -161,11 +234,13 @@ void checkIfTVMuteTriggered(){
       delay(TV_VOLUME_PUSH_DELAY);
     }
 
-    analogWrite(tv_mute_interrupt_led_pin, 0);
+    digitalWrite(tv_mute_interrupt_led_pin, LED_OFF);
+    Serial.print(F("Interrupt: Unmute TV\n"));
   }
 }
 
-
+int inByte;
+bool inLedStatus;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -175,14 +250,49 @@ void setup() {
   pinMode(tv_on_off_output_pin, OUTPUT);
   
   pinMode(tv_current_status_led_pin, OUTPUT);
+  digitalWrite(tv_current_status_led_pin, LED_OFF);
   pinMode(tv_on_off_interrupt_led_pin, OUTPUT);
+  digitalWrite(tv_on_off_interrupt_led_pin, LED_OFF);
   pinMode(tv_mute_interrupt_led_pin, OUTPUT);
+  digitalWrite(tv_mute_interrupt_led_pin, LED_OFF);
   pinMode(tv_check_count_led_pin, OUTPUT);
-  Serial.println("init done");
+  digitalWrite(tv_check_count_led_pin, LED_OFF);
+  inLedStatus = false;
+  Serial.print(F("init done\n"));
 }
 
 void loop() {
   
+  inByte = Serial.read();
+  if (inByte == 'O')
+  {
+    irsend.sendPanasonic(PanasonicAddress, PanasonicPower);
+    if(!inLedStatus) digitalWrite(tv_on_off_interrupt_led_pin, LED_ON);
+    else digitalWrite(tv_on_off_interrupt_led_pin, LED_OFF);
+    inLedStatus = !inLedStatus;
+    Serial.print(F("Backdoor: turn On/off TV\n"));
+  }
+  else if(inByte == 'M')
+  {
+    for (int i = 0; i < TV_VOLUME_DOWN_PUSH; ++i){
+      irsend.sendPanasonic(PanasonicAddress, PanasonicVolumeDown);
+      delay(TV_VOLUME_PUSH_DELAY);
+    }
+    irsend.sendPanasonic(PanasonicAddress, PanasonicMute);
+    
+    digitalWrite(tv_mute_interrupt_led_pin, LED_ON);
+    Serial.print(F("Backdoor: Mute TV\n"));
+  }
+  else if(inByte == 'U')
+  {
+    irsend.sendPanasonic(PanasonicAddress, PanasonicMute);
+    for (int i = 0; i < TV_VOLUME_UP_PUSH; ++i){
+      irsend.sendPanasonic(PanasonicAddress, PanasonicVolumeUp);
+      delay(TV_VOLUME_PUSH_DELAY);
+    }
+    digitalWrite(tv_mute_interrupt_led_pin, LED_OFF);
+    Serial.print(F("Backdoor: Unmute TV\n"));
+  }
   checkIfTVOnOffTriggered();  
   checkIfTVMuteTriggered();
 }
