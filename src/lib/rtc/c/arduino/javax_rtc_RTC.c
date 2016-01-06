15a78a1c59b12fcfa71a3df5e6009909bd6fc0b0
Niels Reijers <nielsreijers@gmail.com>
2016-01-06 23:28:17 +0800

    Issue #151 Added some useful debug methods.

--- src/lib/rtc/c/arduino/javax_rtc_RTC.c
+++ src/lib/rtc/c/arduino/javax_rtc_RTC.c
@@ -7,3 +7,13 @@ void javax_rtc_RTC_void_useRTC_boolean() {
 void javax_rtc_RTC_void_avroraBreak() {
 	asm volatile ("break");
 }
+
+void javax_rtc_RTC_void_avroraPrintShort_short() {
+    int16_t value = dj_exec_stackPopShort();
+    avroraPrintInt16(value);
+}
+
+void javax_rtc_RTC_void_avroraPrintInt_int() {
+    int32_t value = dj_exec_stackPopInt();
+    avroraPrintInt32(value);
+}
--- src/lib/rtc/java/javax/rtc/RTC.java
+++ src/lib/rtc/java/javax/rtc/RTC.java
@@ -4,4 +4,6 @@ public class RTC
 {
 	public static native void useRTC(boolean onOff);
 	public static native void avroraBreak();
+    public static native void avroraPrintShort(short value);
+    public static native void avroraPrintInt(int value);
 }
