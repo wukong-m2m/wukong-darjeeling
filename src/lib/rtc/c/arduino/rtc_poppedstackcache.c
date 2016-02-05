eba67338c046b7274184def2adcce6687b2581bb
Niels Reijers <nielsreijers@gmail.com>
2016-02-05 13:09:12 +0800

    Issue #151 Bugfix for popped stack caching

--- src/lib/rtc/c/arduino/rtc_poppedstackcache.c
+++ src/lib/rtc/c/arduino/rtc_poppedstackcache.c
@@ -319,8 +319,8 @@ bool rtc_stackcache_getfree_16bit_prefer_ge_R16(uint8_t *regs) {
 void rtc_stackcache_push_pair(uint8_t reg_base, uint8_t which_stack, bool is_int_l) {
     uint8_t idx = REG_TO_ARRAY_INDEX(reg_base);
     if (RTC_STACKCACHE_IS_IN_USE(idx)
-    || (RTC_STACKCACHE_IS_AVAILABLE(idx) && (RTC_STACKCACHE_GET_VALUETAG(idx) == rtc_ts->current_instruction_valuetag
-                                             || (RTC_STACKCACHE_GET_VALUETAG(idx) == RTC_VALUETAG_TO_INT_L(rtc_ts->current_instruction_valuetag) && is_int_l)))) {
+        || reg_base == R22 || reg_base == R24
+        || (RTC_STACKCACHE_IS_AVAILABLE(idx) && (RTC_STACKCACHE_GET_VALUETAG(idx) == rtc_ts->current_instruction_valuetag || (RTC_STACKCACHE_GET_VALUETAG(idx) == RTC_VALUETAG_TO_INT_L(rtc_ts->current_instruction_valuetag) && is_int_l)))) {
         // shift depth for all pairs on the stack
         for (uint8_t idx=0; idx<RTC_STACKCACHE_MAX_IDX; idx++) {
             if (RTC_STACKCACHE_IS_ON_STACK(idx)) {
