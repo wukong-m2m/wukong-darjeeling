c0a1d36581871ed127b19020f306ed2ecec604ac
B.L.Tsai <b95008@csie.ntu.edu.tw>
2016-02-13 03:37:32 +0800

    Fix tool to remove node

--- wukong/tools/python/remove_node.py
+++ wukong/tools/python/remove_node.py
@@ -23,14 +23,14 @@ print "\n1.Gateway DB"
 db = MPTN.DBDict(os.path.join(os.path.abspath(master), "master_gateway_info.sqlite"))
 if len(db) == 0: print "None"
 else:
+    if to_del_node_id in db:
+        is_id_gateway = True
+        del db[to_del_node_id]
     # Gateway(id=167772166, tcp_address=('127.0.0.1', 9001), if_address=167772166, if_address_len=4, prefix=167772160, prefix_len=24,
     #         network=IPv4Network('10.0.0.0/24'), network_size=256, netmask=4294967040, hostmask=255,
     #         uuid='\xe5\xa8\xc3\xc6\xa8\xcdB;\x86\xfb\xc5O\\x\xad\xd4')
     for (gateway_id, gateway) in db.iteritems():
         print "Gateway ID:", MPTN.ID_TO_STRING(int(gateway_id)), "IF_ADDR:", MPTN.ID_TO_STRING(gateway.if_address), "IF_ADDR_LEN:", gateway.if_address_len, "PREFIX:", MPTN.ID_TO_STRING(gateway.prefix), "PREFIX_LEN:", gateway.prefix_len, "NETWORK:", str(gateway.network), "NETWORK_SIZE:", gateway.network_size, "NETMASK:", MPTN.ID_TO_STRING(gateway.netmask), "HOSTMASK:", MPTN.ID_TO_STRING(gateway.hostmask), "UUID:", map(ord, gateway.uuid), "TCP_ADDRESS", str(gateway.tcp_address)
-
-    if to_del_node_id in db:
-        is_id_gateway = True
 
 print "\n2.Node DB"
 db = MPTN.DBDict(os.path.join(os.path.abspath(master), "master_node_info.sqlite"))
@@ -40,20 +40,16 @@ else:
         del db[to_del_node_id]
     to_be_deleted = []
     for (node_id, node) in db.iteritems():
-        print "Node ID:", MPTN.ID_TO_STRING(int(node_id)), "IF_ADDR:", MPTN.ID_TO_STRING(node.if_address), "IS_GATEWAY:", node.is_gateway, "GATEWAY ID:", MPTN.ID_TO_STRING(node.gateway_id), "UUID:", map(ord, node.uuid)
-
-        if (node.gateway_id == to_del_node_id):
-            to_be_deleted.append(int(node_id))
-
-    if is_id_gateway:
-        for node_id in to_be_deleted:
+        if is_id_gateway and (node.gateway_id == to_del_node_id):
             del db[node_id]
+            continue
+
+        print "Node ID:", int(node_id), MPTN.ID_TO_STRING(int(node_id)), "IF_ADDR:", MPTN.ID_TO_STRING(node.if_address), "IS_GATEWAY:", node.is_gateway, "GATEWAY ID:", MPTN.ID_TO_STRING(node.gateway_id), "UUID:", map(ord, node.uuid)
 
 sys.path.remove(os.path.abspath(master))
 
 if is_id_gateway:
     print "The ID is gateway. Done removing all nodes related to that gateway.\nPlease remove the *.sqlite, *.pkl within that gateway folder"
-    exit()
 
 import sys, os
 tools = os.path.join(os.path.dirname(__file__), "..")
@@ -69,6 +65,9 @@ db = MPTN.DBDict(os.path.join(os.path.abspath(gateway), "gtw_nexthop_table.sqlit
 if len(db) == 0: print "None"
 else:
     for (network_string, tcp_address) in db.iteritems():
+        if is_id_gateway and MPTN.IS_ID_IN_NETWORK(to_del_node_id, MPTN.ID_NETWORK_FROM_STRING(network_string)):
+            del db[network_string]
+            continue
         print "TCP address of ID/prefix_len:", network_string, "is", tcp_address
 
 print "\n2.Address DB"
@@ -81,7 +80,7 @@ else:
             del db[address]
             break
         else:
-            print "Registered address and its UUID:", MPTN.ID_TO_STRING(int(address)), map(ord, uuid)
+            print "Registered address and its UUID:", int(address), MPTN.ID_TO_STRING(int(address)), map(ord, uuid)
 
 print "\n3.Settings DB"
 db = MPTN.DBDict(os.path.join(os.path.abspath(gateway), "gtw_settings_db.sqlite"))
@@ -89,6 +88,9 @@ if len(db) == 0: print "None"
 else:        
     for (key,value) in db.iteritems():
         print "The value of setting", key, "is", value
+
+if to_del_node_id & 0xffffff00 != db["GTWSELF_ID"] & 0xffffff00:
+    exit()
 
 if CONFIG.TRANSPORT_INTERFACE_TYPE == 'udp':
     from transport_udp import UDPDevice
@@ -100,13 +102,15 @@ if CONFIG.TRANSPORT_INTERFACE_TYPE == 'udp':
         f.close()
         index = None
         for i, d in enumerate(devices):
-            print d.host_id, d.ip, d.port
             if d.host_id == (to_del_node_id & 0xff):
                 index = i
                 break
 
         if index is not None:
             devices.pop(index)
+
+        for i, d in enumerate(devices):
+            print d.host_id, d.ip, MPTN.ID_TO_STRING(d.ip), d.port
 
         f = open(os.path.join(os.path.abspath(gateway), "devices.pkl"),'w')
         pickle.dump(devices,f)
