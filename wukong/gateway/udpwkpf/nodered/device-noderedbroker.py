31c47eb2462fc9c5cd4659d9157f96c2e8675970
wukong <iapyeh@gmail.com>
2016-04-14 16:34:17 +0800

    Bug fix for multiple WuKong senders.

--- wukong/gateway/udpwkpf/nodered/device-noderedbroker.py
+++ wukong/gateway/udpwkpf/nodered/device-noderedbroker.py
@@ -7,198 +7,17 @@ from twisted.internet import reactor
 from twisted.internet.protocol import Factory,Protocol
 from twisted.internet.endpoints import TCP4ServerEndpoint
 
-def debug(*s,**kw):
+debug = None
+
+def verbose(*s,**kw):
     newline= kw.get('newline','\n')
     cols = [str(x) for x in s]
     line = '\t'.join(cols).encode('utf-8')+newline
     sys.stderr.write(line)
+def silent(*s,**kw):
+    #sys.stderr.write('SILENT\n')
+    pass
 
-"""
-class UIButton(WuClass):
-    def __init__(self):
-        self.loadClass('UIButton')
-    def udpate(self,obj,pID,val):
-        pass
-    def init(self):
-        pass
-
-class UISlider(WuClass):
-    def __init__(self):
-        self.loadClass('UISlider')
-    def udpate(self,obj,pID,val):
-        pass
-    def init(self):
-        pass
-class UIScene(WuClass):
-    def __init__(self):
-        self.loadClass('UIScene')
-    def udpate(self,obj,pID,val):
-        pass
-    def init(self):
-        pass
-class HsremoteDevice(Device):
-    def __init__(self,gtwip,selfip):
-        Device.__init__(self,gtwip,selfip)
-    def init(self):
-        print "xxxxxx"
-        cls = UIButton()
-        self.addClass(cls,self.FLAG_APP_CAN_CREATE_INSTANCE| self.FLAG_VIRTUAL)
-        cls = UISlider()
-        self.addClass(cls,self.FLAG_APP_CAN_CREATE_INSTANCE| self.FLAG_VIRTUAL)
-        cls = UIScene()
-        self.addClass(cls,self.FLAG_APP_CAN_CREATE_INSTANCE| self.FLAG_VIRTUAL)
-        self.ID_slider = cls.getWuClassID('UISlider')
-        self.ID_scene = cls.getWuClassID('UIScene')
-        pass
-    def update(self,obj,pID,val):
-        pass
-    def findSlider(self,p,d):
-        for i in range(0,len(self.objects)):
-            obj = self.objects[i]
-            if obj.cls.ID == self.ID_slider:
-                print obj.port,obj.getProperty(1),obj.getProperty(2)
-                if obj.getProperty(1) == p and obj.getProperty(2) == d:
-                    return obj
-        return None
-    def findScene(self,p,d):
-        for i in range(0,len(self.objects)):
-            obj = self.objects[i]
-            if obj.cls.ID == self.ID_scene:
-                if obj.getProperty(1) == p and obj.getProperty(2) == d:
-                    return obj
-        return None
-
-class SearchProtocol(DatagramProtocol):
-    def datagramReceived(self, data, (host, port)):
-        # \x01\x00\x00\xc0\x00\x00\x00\x00\x00\x01\xff\xff
-        net = netifaces.ifaddresses(myinterface)
-        mac = net[netifaces.AF_LINK]
-        ip = map(int,net[netifaces.AF_INET][0]['addr'].split('.'))
-        print ip
-        mac = netifaces.ifaddresses(myinterface)[netifaces.AF_LINK][0]['addr'].split(':')
-        print mac
-        
-        header = struct.pack("hBBIhh6B",1,0,0xc0,0,1,2,int(mac[0],16),int(mac[1],16),int(mac[2],16),int(mac[3],16),int(mac[4],16),int(mac[5],16))
-        data = header+struct.pack("4BII", ip[0],ip[1],ip[2],ip[3],0,0)+"wukong"+"\0"*14
-        self.transport.write(data,(host,port))
-        print map(ord, data)
-        print "receive data %r from %s:%d" % (data,host,port)
-class Cgi(Protocol):
-    isLeaf = True
-    def __init__(self,factory):
-        self.factory = factory
-    def dataReceived(self,request):
-        print request
-        # /cgi-bin/proxy?cmd=/scene&p=1&d=113&serialno=,65535
-        # GET /cgi-bin/proxy?cmd=/scene&p=1&d=113&serialno=,65535 HTTP/1.
-        lines = request.split('\n')
-        print lines[0]
-        tokens = lines[0].split(' ')
-        ulist = tokens[1].split('?')
-        if len(ulist) >= 2:
-            paths = ulist[0].split('/')
-            pars = ulist[1].split('&')
-            query = {}
-            for p in pars:
-                ff = p.split('=')
-                if len(ff) != 2:
-                    return self.render_none()
-                query[ff[0]] = ff[1]
-        else:
-            paths = ulist[0].split('/')
-            query={}
-        print paths
-        if tokens[0] == 'GET':
-            if paths[1] == 'cgi-bin':
-                if paths[2] == 'proxy':
-                    print query
-                    if query['cmd'] == '/scene':
-                        self.render_scene(query)
-                    elif query['cmd'][0:2] == '/r':
-                        self.render_control(query['cmd'])
-                    else:
-                        self.render_none()
-                else:
-                    self.render_none()
-            else:
-                self.render_none()
-        elif tokens[0] == 'POST':
-            print 'POST method',paths
-            if paths[1] == 'cgi-bin':
-                if paths[2] == 'ssidlist':
-                    print lines
-                    for i in range(len(lines)):
-                        if lines[i] == '' or lines[i] == '\r':
-                            try:
-                                obj = cjson.decode('\n'.join(lines[i+1:]))
-                                print obj
-                            except:
-                                pass
-            self.render_none()
-
-    def doCommand_clear(self):
-        if self.clear_obj:
-            self.clear_obj.setProperty(0,7)
-            self.clear_obj.setProperty(3,False)
-            self.clear_obj = None
-    def doCommand(self,d,p,v):
-        obj = self.factory.device.findSlider(d,p)
-        if obj:
-            if v == 30: #up
-                obj.setProperty(0,3)
-            elif v == 31: #down
-                obj.setProperty(0,4)
-            elif v == 32: #left
-                obj.setProperty(0,1)
-            elif v == 33: #right
-                obj.setProperty(0,2)
-            elif v == 19: #O
-                obj.setProperty(0,5)
-            elif v == 21: #X
-                obj.setProperty(0,6)
-            elif v == 20: #Stop
-                obj.setProperty(0,0)
-            elif v == 39: #change mode
-                obj.setProperty(3,True)
-            if d == 11 or d == 12 or d == 13 or  d == 14:
-                print "CMD: %d reset" % (d)
-                self.clear_obj  = obj
-                reactor.callLater(0.5, self.doCommand_clear)
-    def doScene(self,s):
-        obj = self.factory.device.findScene(1,s)
-        if obj: obj.setProperty(0,v)
-    def render_control(self,cmd):
-        cmd = cmd[2:].split(',')
-        print ' command %0x' % int(cmd[0])
-        arg = int(cmd[0])
-        v = (arg >> 16)&0xff
-        d = (arg>>8)&0xff
-        p = arg & 0xff
-        print "CMD (%d,%d) = %d" % (d,p,v)
-        self.doCommand(d,p,v)
-        self.transport.write('HTTP/1.1 200 OK\nContent-Length: 0\n\n')
-        self.transport.loseConnection()
-
-    def render_scene(self,query):
-        d = int(query['d'])
-        p = int(query['p'])
-        print 'render_scene'
-        if p == 1 and (d>=113 & d <= 117):
-            s = d-113+1
-            print 'Do scene %d' % s
-            self.doScene(s)
-        self.transport.write('HTTP/1.1 200 OK\nContent-Length: 0\n\n')
-        self.transport.loseConnection()
-
-    def render_none(self):
-        self.transport.write('HTTP/1.1 200 OK\nContent-Length: 1\n\n0')
-        self.transport.loseConnection()
-class CgiFactory(Factory):
-    def __init__(self,device):
-        self.device = device
-    def buildProtocol(self, addr):
-        return Cgi(self)
-"""
 class MessageQueue(object):
     def __init__(self):
         self.queue = []
@@ -206,45 +25,45 @@ class MessageQueue(object):
         self._timer = None
         self._message_ttl = 3.0 ## 3 seconds
     def addMessage(self,message):
-        #debug('add out message',message['payload'])
+        debug('add out message',message['payload'])
         self.queue.append(message)
         if self._timer is None:
             self._timer = reactor.callLater(0.5,self._notify)
     def _notify(self):
         #
         # Consume the outgoing queue
+        # do cleanup of the queue at the same time
         #
-        self._timer = None
-
-        ## do cleanup of the queue
         now = time.time()
         item_tobe_remove = []
         for item in self.queue:
             
             topic = item['payload']['topic']
             
+            if now - item['ts'] > self._message_ttl:
+                item_tobe_remove.append(item)
+                continue
+                debug('drop out message ',item['payload'])
+
             try:
                 listeners = self.listeners[topic]
             except KeyError:
                 listeners = []
-                
+
             for listener in listeners:
                 try:
                     listener(item)
                 except:
                     debug('NodeRedBroker Listener Error:%s' % traceback.format_exc())
             
-            if now - item['ts'] > self._message_ttl:
-                item_tobe_remove.append(item)
-                #debug('drop out message ',item['payload'])
-            elif item['ack_count'] >= len(listeners):
-                item_tobe_remove.append(item)
-                #debug('cleanup out message',item['payload'])
+            item_tobe_remove.append(item)
+            debug('cleanup out message',item['payload'])
 
         item_tobe_remove.reverse()
         for item in item_tobe_remove:
             self.queue.remove(item)
-        #debug('out message length',len(self.queue),'listener number',len(self.listeners))
+        debug('%s message removed' % len(item_tobe_remove),', queue length',len(self.queue),',listener length',len(self.listeners))
+        self._timer = None
 
     def addListener(self,topic,_callable):
         try:
@@ -252,10 +71,15 @@ class MessageQueue(object):
         except KeyError:
             self.listeners[topic] = [_callable]
     def removeListener(self,_callable):
+        remove_topic = False
         for topic,callables in self.listeners.iteritems():
             if _callable in callables:
                 callables.remove(_callable)
-
+                remove_topic = len(callables)==0
+                break
+        if remove_topic:
+            del self.listeners[topic]
+        debug('after removed listetners:%s' % len(self.listeners))
 global message_out_queue
 message_out_queue = MessageQueue()
 
@@ -264,16 +88,15 @@ class NodeREDSignalSender(WuClass):
     Receive signal from WuKong, then pass to NodeRED
     """
     def __init__(self):
-        print 'NodeREDSignalSender __init__()'
         WuClass.__init__(self)
         self.loadClass('NodeREDSignalSender')
     def update(self,obj,pID,val):
-        print 'NodeREDSignalSender update',[obj,pID,val]
+        debug('NodeREDSignalSender update',[obj,pID,val])
         if pID==0:
             #
             # This is setting the topic, doesn't need to send to remote, 
             #
-            setattr(obj.cls,'topic',val)
+            setattr(obj,'topic',val)
         else:
             global message_out_queue
             message_out_queue.addMessage(self.createMessage(obj,pID,val))
@@ -291,12 +114,11 @@ class NodeREDSignalSender(WuClass):
         }
         message = {
             'payload':{
-                'topic':getattr(obj.cls,'topic'),
+                'topic':getattr(obj,'topic'),
                 'type':signalType[pID],
                 'value':val
             },
-            'ts':time.time(),
-            'ack_count':0
+            'ts':time.time()
         }
         return message
 
@@ -308,11 +130,10 @@ class NodeREDSignalReceiver(WuClass):
     subscribers = weakref.WeakSet()
     def __init__(self):
         WuClass.__init__(self)
-        print 'NodeREDSignalReceiver __init__()'
         self.topic = 0 ## default topic
         self.loadClass('NodeREDSignalReceiver')
     def update(self,obj,pID,val):
-        #print 'NodeREDSignalReceiver update',[obj,pID,val]
+        debug('NodeREDSignalReceiver update',[obj,pID,val])
         WuClass.update(self,obj,pID,val)
         if pID==0:
             #
@@ -324,7 +145,7 @@ class NodeREDSignalReceiver(WuClass):
                 NodeREDSignalReceiver.subscribers.add(obj)
             return
     def init(self):
-        print 'NodeREDSignalReceiver init'
+        pass
     def __repr__(self):
         return '<NodeREDSignalReceiver>'
     @classmethod
@@ -362,33 +183,14 @@ class NodeREDBroker(Device):
     def __init__(self,gtwip,selfip):
         Device.__init__(self,gtwip,selfip)
     def init(self):
-        print "NodeRedBroker init()"
         cls = NodeREDSignalReceiver()
         self.addClass(cls,self.FLAG_APP_CAN_CREATE_INSTANCE| self.FLAG_VIRTUAL)
         cls = NodeREDSignalSender()
         self.addClass(cls,self.FLAG_APP_CAN_CREATE_INSTANCE| self.FLAG_VIRTUAL)
     def update(self,obj,pID,val):
-        print 'NodeREDBroker.update()',[obj,pID,val]
+        debug('NodeREDBroker.update()',[obj,pID,val])
+        pass
 
-def parseArguments():
-    from optparse import OptionParser,OptionGroup
-    parser = OptionParser(usage="""
-  $python %prog -g 192.168.56.101 -p 192.168.56.101:5100
-  or,
-  $sh run.sh -g 192.168.56.101 -p 192.168.56.101:5100
-    """,
-    description="This is the NodeRED Broker for WuKong",
-    )
-    parser.add_option('-g','--gateway',
-        help='IP of the gateway')
-    parser.add_option('-p','--ip',
-        help='ip:port of this device, ex. -p 192.168.56.101:5100')
-    parser.add_option('-d','--develop',action="store_true",
-        help='use developing paramters: -g 192.168.56.101 -p 192.168.56.101:5100')
-
-    (options, args) = parser.parse_args()
-
-    return options,args,parser
 
 #
 # Communication to NodeRED
@@ -460,7 +262,6 @@ class WuKongNodeREDProtocol(Protocol):
             content = WuKongNodeREDProtocol.boundary+cjson.encode(item['payload'])+WuKongNodeREDProtocol.boundary+'\n'
             self.transport.write(content)
             self.last_message_out_ts = item['ts']
-            item['ack_count'] += 1
             debug('sending %s' % content)
         
 
@@ -478,15 +279,39 @@ def startServerForNodeRED(noderedBroker,port=5101):
 # This "global" can be removed in productive environment
 #
 global noderedBroker
-def main(gtwip,selfip):
+def main(gtwip,selfip,options):
     
     global noderedBroker
     noderedBroker = NodeREDBroker(gtwip,selfip)
     
     startServerForNodeRED(noderedBroker,port=5101)
-    
-    print "NodeRed Broker is ready"
-    #reactor.callLater(1.5,simulator)
+   
+    if options.simulator:
+        reactor.callLater(1.5,simulator)
+
+def parseArguments():
+    from optparse import OptionParser,OptionGroup
+    parser = OptionParser(usage="""
+      $python %prog -g 192.168.56.101 -p 192.168.56.101:5100
+      or,
+      $sh run.sh -g 192.168.56.101 -p 192.168.56.101:5100
+        """,
+        description="This is the NodeRED Broker for WuKong",
+    )
+    parser.add_option('-g','--gateway',
+        help='IP of the gateway')
+    parser.add_option('-p','--ip',
+        help='ip:port of this device, ex. -p 192.168.56.101:5100')
+    parser.add_option('-d','--develop',action="store_true",
+        help='use developing paramters: -g 192.168.56.101 -p 192.168.56.101:5100')
+    parser.add_option('-s','--simulator',action="store_true",
+        help='run the simulator')
+    parser.add_option('-v','--verbose',action="store_true",
+        help='verbosely dumping message')
+
+    (options, args) = parser.parse_args()
+
+    return options,args,parser
 
 def simulator():
     """
@@ -494,12 +319,16 @@ def simulator():
     """
     import random
     global noderedBroker
-    print "Simulator is running"
+    debug("Simulator is running")
     
     # create a wuobject 
     #NodeREDSignalSender_cls = noderedBroker.classes[21001]
     #NodeREDSignalReceiver_cls = noderedBroker.classes[21002]
+    
+    # 2 sender
     noderedBroker.addObject(21001)
+    noderedBroker.addObject(21001)
+    # 1 receiver
     noderedBroker.addObject(21002)
     
     TOPIC_PROPERTY = 0
@@ -508,7 +337,8 @@ def simulator():
     STRING_SIGNAL_PROPERTY = 3
 
     SIGNAL_SENDER = 1
-    SIGNAL_SENDER_TOPIC_VALUE = 1
+    SIGNAL_SENDER1_TOPIC_VALUE = 1
+    SIGNAL_SENDER2_TOPIC_VALUE = 3
 
     SIGNAL_RECEIVER = 2
     SIGNAL_RECEIVER_TOPIC_VALUE = 2
@@ -518,18 +348,35 @@ def simulator():
     #noderedBroker.setProperty(SIGNAL_RECEIVER,TOPIC_PROPERTY,SIGNAL_RECEIVER_TOPIC_VALUE)
 
     # set topic
-    signal_sender_object = noderedBroker.objects[0]
-    signal_sender_object.cls.update(signal_sender_object,TOPIC_PROPERTY,SIGNAL_SENDER_TOPIC_VALUE)
+    signal_sender1_object = noderedBroker.objects[0]
+    signal_sender1_object.cls.update(signal_sender1_object,TOPIC_PROPERTY,SIGNAL_SENDER1_TOPIC_VALUE)
+
+    signal_sender2_object = noderedBroker.objects[1]
+    signal_sender2_object.cls.update(signal_sender2_object,TOPIC_PROPERTY,SIGNAL_SENDER2_TOPIC_VALUE)
+
     def send_signal():
-        signal_sender_object = noderedBroker.objects[0]
-        value = random.randint(0,100)
-        signal_sender_object.cls.update(signal_sender_object,INTEGER_SINGAL_PROPERTY,value)
-        reactor.callLater(10,send_signal)
+        v = random.randint(0,100)%3
+        def send1():
+            signal_sender1_object = noderedBroker.objects[0]
+            value = random.randint(0,100)
+            signal_sender1_object.cls.update(signal_sender1_object,INTEGER_SINGAL_PROPERTY,value)
+        def send2():
+            signal_sender2_object = noderedBroker.objects[1]
+            value = random.randint(0,100)
+            signal_sender2_object.cls.update(signal_sender2_object,INTEGER_SINGAL_PROPERTY,value)
+        if v==0:
+            send1()
+        elif v==1:
+            send2()
+        else:
+            send1()
+            send2()
+        reactor.callLater(3,send_signal)
     
     reactor.callLater(1,send_signal)
     
     def receiving_signal():
-        signal_receiver_object = noderedBroker.objects[1]
+        signal_receiver_object = noderedBroker.objects[2]
         signal_receiver_object.cls.update(signal_receiver_object,TOPIC_PROPERTY,SIGNAL_RECEIVER_TOPIC_VALUE)
 
     receiving_signal()
@@ -544,6 +391,10 @@ if __name__ == "__main__":
     #reactor.listenUDP(8000, SearchProtocol())
 
     options,args,parser = parseArguments()
+    if options.verbose:
+        debug = verbose
+    else:
+        debug = silent
     if options.develop:
         gtwip = '192.168.56.101'
         selfip = gtwip+':5100'
@@ -555,7 +406,7 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(1)
     else:
-        sys.stderr.write('NodeRED Broker is running with:\n')
-        sys.stderr.write('    Gateway:%s\n    Broker ip and port:%s\n' % (gtwip,selfip))
-        reactor.callWhenRunning(main,gtwip,selfip)
+        debug('NodeRED Broker is running with:\n')
+        debug('    Gateway:%s\n    Broker ip and port:%s\n' % (gtwip,selfip))
+        reactor.callWhenRunning(main,gtwip,selfip,options)
         reactor.run()
--- wukong/gateway/udpwkpf/nodered/run.sh
+++ wukong/gateway/udpwkpf/nodered/run.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=../
-python device-noderedbroker.py $1 $2
+python device-noderedbroker.py $1 $2 $3 $4 $5 %6 %7
