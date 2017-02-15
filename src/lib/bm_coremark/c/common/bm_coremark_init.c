e1a14f1088f1e383a34c5c0d714be8840e2915bf
Niels Reijers <nielsreijers@gmail.com>
2017-02-15 16:52:13 +0800

    Issue #151 Add empty benchmark for EEMBC Core Mark

---
+++ src/lib/bm_coremark/c/common/bm_coremark_init.c
@@ -0,0 +1,1 @@
+void bm_coremark_init(void) {}
---
+++ src/lib/bm_coremark/java/javax/rtcbench/RTCBenchmark.java
@@ -0,0 +1,33 @@
+package javax.rtcbench;
+
+import javax.darjeeling.Stopwatch;
+
+public class RTCBenchmark {
+    public static String name = "COREMARK";
+    public static native void test_native();
+    public static boolean test_java() {
+        rtcbenchmark_measure_java_performance();
+        return true;
+    }
+
+    public static void rtcbenchmark_measure_java_performance() {
+        Stopwatch.resetAndStart();
+
+        for (short i=0; i<100; i++) {
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+            nothing();
+        }
+
+        Stopwatch.measure();
+    }
+
+    public static void nothing() {}
+}
