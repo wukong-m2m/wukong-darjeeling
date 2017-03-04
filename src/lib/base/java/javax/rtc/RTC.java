e19ab8204db2a664ee7f101c3a3b564e10a881fc
Niels Reijers <nielsreijers@gmail.com>
2017-03-04 21:21:07 +0200

    Issue #151 Added function in RTC class to do debug hex prints from Java

--- src/lib/base/java/javax/rtc/RTC.java
+++ src/lib/base/java/javax/rtc/RTC.java
@@ -6,6 +6,8 @@ public class RTC
 	public static native void avroraBreak();
     public static native void avroraPrintShort(short value);
     public static native void avroraPrintInt(int value);
+    public static native void avroraPrintHexShort(short value);
+    public static native void avroraPrintHexInt(int value);
     public static native void avroraPrintSP();
 
 	public static native void beep(int number);
