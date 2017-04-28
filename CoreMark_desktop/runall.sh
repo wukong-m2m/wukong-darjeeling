21876601c94f1961ba2d21603caf94438d52e5c9
Niels Reijers <nielsreijers@gmail.com>
2017-04-28 13:27:48 +0800

    Issue #151 Use Proguard to optimise desktop version as well

--- CoreMark_desktop/runall.sh
+++ CoreMark_desktop/runall.sh
@@ -10,20 +10,20 @@ date >> output.txt
 # ori
 for i in `seq 1 10`; {
 	echo 'ori' $i >> output.txt
-	java -jar java/build/libs/coremk_ori.jar-1.0.jar | grep -E 'Iteration|Total' >> output.txt
+	java -jar java/build/libs/pg_coremk_ori.jar | grep -E 'Iteration|Total' >> output.txt
 	echo '' >> output.txt
 }
 
 # opt
 for i in `seq 1 10`; {
 	echo 'opt' $i >> output.txt
-	java -jar java/build/libs/coremk_opt.jar-1.0.jar | grep -E 'Iteration|Total' >> output.txt
+	java -jar java/build/libs/pg_coremk_opt.jar | grep -E 'Iteration|Total' >> output.txt
 	echo '' >> output.txt
 }
 
 # cht
 for i in `seq 1 10`; {
 	echo 'cht' $i >> output.txt
-	java -jar java/build/libs/coremk_cht.jar-1.0.jar | grep -E 'Iteration|Total ticks' >> output.txt
+	java -jar java/build/libs/pg_coremk_cht.jar | grep -E 'Iteration|Total ticks' >> output.txt
 	echo '' >> output.txt
 }
