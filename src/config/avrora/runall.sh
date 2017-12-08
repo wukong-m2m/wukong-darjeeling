1ec5a39222d2ec819a35094ad92c5b2d93e9b5f8
Niels Reijers <nielsreijers@gmail.com>
2017-12-08 17:27:18 +0800

    Run both safe and unsafe versions in runall.sh

--- src/config/avrora/runall.sh
+++ src/config/avrora/runall.sh
@@ -32,8 +32,12 @@ gdj clean
 # BEST CASE ONLY
 for benchmark in ${benchmarks}
 do
+    # SAFE
     gdj avrora_store_trace -Paotbm=${benchmark} -Paotstrat=markloop         -Puseconstantshiftoptimisation=true  -Puse16bitarrayindex=true  -Pusesimul=true
+    # UNSAFE
+    gdj avrora_store_trace -Paotbm=${benchmark} -Paotstrat=markloop         -Puseconstantshiftoptimisation=true  -Puse16bitarrayindex=true  -Pusesimul=true -Paddsafetychecks=false
 done
+
 
 # MARKLOOP + XXTEA FOR DIFF NUMBERS OF PINNED REGISTERS
 # for benchmark in ${benchmarks}
