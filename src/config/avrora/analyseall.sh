3d36b010506c8dbf075bd7b54f2e65b24e440c39
Niels Reijers <nielsreijers@gmail.com>
2017-12-17 16:26:07 +0800

    Made F# code tail recursive to avoid stack overflow

--- src/config/avrora/analyseall.sh
+++ src/config/avrora/analyseall.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-mono32 process-output/process-output/bin/Debug/process-output.exe process all `pwd`
-mono32 process-output/process-output/bin/Debug/process-output.exe combine all `pwd`
+mono process-output/process-output/bin/Debug/process-output.exe process all `pwd`
+mono process-output/process-output/bin/Debug/process-output.exe combine all `pwd`
